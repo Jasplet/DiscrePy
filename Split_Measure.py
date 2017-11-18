ecdc8666052c22cdd79b8335aae45298666753b0
Joseph Asplet <32487558+Jasplet@users.noreply.github.com>
2017-11-18 13:04:34 +0000

    Uploaded Scripts in current working condition

---
+++ Split_Measure.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+## This is a script which takes downloaded traces and Calculates SKS splitting
+import obspy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import obspy.signal.filter as filt
+import obspy.core.utcdatetime
+import glob
+import os.path
+import splitwavepy as sw
+from obspy.clients.fdsn import Client
+from obspy.taup import TauPyModel
+
+#### Reading downloaded traces and Windowing data
+# Generate Travel time model for SKS phase for all events prior to Windowing
+model = obspy.taup.tau.TauPyModel(model="iasp91")
+
+# arrival = model.get_travel_times(depth,dist,["SKS"])
+# no_N = int(os.popen("ls *BHN.sac | wc -l").read())
+# no_E = int(os.popen("ls *BHE.sac | wc -l").read())
+# no_Z = int(os.popen("ls *BHZ.sac | wc -l").read())
+# num_s = max(no_N,no_E) # Find which channel has the max number of traces downloaded (i.e what is the maximum number of events we can measure splitting for)
+
+output_file = open('NEW_Splitting.txt','w')
+output_file.write('ID YEAR MON DAY HOUR MIN SEC STAT FAST DFAST TLAG DTLAG WBEG WEND \n')
+
+
+for i in range(99,100):
+    st_id = "NEW_" + str(i).zfill(2) + "_" + "*.sac" # Generate expected file names. Wildcard used to catch all 3 channels
+
+    filename = "NEW_Splitting_" + str(95).zfill(2) + ".eigm"
+
+    try:
+        st = obspy.core.stream.read(st_id) # Reads all traces that match st_id
+        work = True
+
+    except Exception:
+        print("Exception Encountered for event")
+        row = str(i)+' N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A \n'
+        work = False
+
+    if (work == True): #Traces for event have been succesfully read so lets try to measure splittiing!
+        arrival = model.get_travel_times(st[0].stats.sac.evdp,st[0].stats.sac.gcarc,["SKS"]) # Calculates expected SKS arrival for the current event
+        time = st[0].stats.starttime # Start time of stream for event. This should be the event origin time.
+        print(time) # Prints origin time.
+        st.filter("bandpass",freqmin=0.01, freqmax= 0.5,corners=2,zerophase=True)
+        SKS = arrival[0].time
+        t0 = obspy.core.utcdatetime.UTCDateTime(st[0].stats.starttime + SKS) # SKS arrival time relative to trace start as a UTCDateTime object
+        st.trim(0,t0+300)
+
+        east = st[0].data #East compenent seismogram, ready to be put into a Pair object.
+        north = st[1].data #North compenent seismogram, ready to be put into a Pair object.
+        sample_interval = st[0].stats.delta
+        split_pair = sw.Pair(north,east,delta = sample_interval) # Creates the Pair object
+        #split_pair.set_window(t0-30,t0+30)
+        print("SKS arrival time for event","is ",SKS)
+        split_pair.plot(pick=True,marker=SKS) # Plots traces and particle motion, with a marker for the SKS arrival. Plot window also allows manual picking of the window.
+        window1 = split_pair.wbeg()
+        window2 = split_pair.wend()
+        print("Window Begins at",window1,"and ends at",window2)
+        split = sw.EigenM(split_pair,lags=(4,) )
+        split.save(filename)
+
+
+        date = str(time.year)+" "+ str(time.month).zfill(2)+" "+str(time.day).zfill(2) +" " # Format date information so that is inteligible when output
+        t = str(time.hour).zfill(2)+" "+str(time.minute).zfill(2)+" "+str(time.second).zfill(2) # Formating time infomation for printing
+        row = str(i)+' '+date+' '+t+' '+str(stat)+' '+str(split.fast)+' '+str(split.dfast)+' '+str(split.tlag)+' '+str(split.dtlag)+'\n' #Row of data to be written to output textfile
+    output_file.write(row)
+
+output_file.close()
+
+#data = [split1.FAST.data,split1.TLAG.data,fast.data,tlag.data]
+#print(data)
+# index = range(0,len(split1.FAST))
+# columns = ["Jacks_Fast","Jacks_Lag","My_Fast","My_Lag"]
+# df = pd.DataFrame(index=index,columns=columns)
+# df = df.fillna(0)
+# df.Jacks_Fast = data[0]
+# df.Jacks_Lag = data[1]
+# df.My_Fast = data[2]
+# df.My_Lag = data[3]
+# print(df)
+# fast_lag = df[(df['My_Fast'] != 0) & (df['My_Lag'] != 0)]
+# print(fast_lag)
---
+++ Split_Read.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python
+## This is a script to read the SKS splitting data from Walpole et al 2014 and manipulate that data.
+
+import obspy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import obspy.signal.filter as filt
+import obspy.core.utcdatetime
+import glob
+import os.path
+import splitwavepy as sw
+from obspy.clients.fdsn import Client
+from obspy.taup import TauPyModel
+from obspy.clients.fdsn.header import (FDSNException)
+from obspy.clients.fdsn.header import (FDSNNoDataException)
+from obspy.core import AttribDict
+from obspy.clients import iris
+
+
+##################### Start of Function to Request and Save Traces ##################
+def trace_download(dates,times,evla,evlo,stla,stlo,station, n_events):
+    ## Function to download and save traces for a pre-determined set of events
+    too_short = 0
+    no_dat = 0
+    for i in range(0,n_events):
+        # Test if trace is already downloaded and create a UTCDateTime object for the Starttime
+        ps = str(dates[i]) + "T" + str(times[i]).zfill(4)
+        start = obspy.core.UTCDateTime(ps) #iso8601=True
+        yr = str(start.year)
+        mn_tst = str(start.month).zfill(2) # Converts Month to string. zfill ensures it is 2 characters by adding a leading zero if required
+        d_tst = str(start.day).zfill(2)   # Day of event, this is to contruct expected
+        h_tst = str(start.hour).zfill(2) # Converts Hour to string. zfill ensures it is 2 characters by adding a leading zero if required
+        min_tst = str(start.minute).zfill(2)
+
+        client = obspy.clients.fdsn.Client("IRIS")
+        try:
+            cat = client.get_events(starttime=start-60,endtime=start+60 ,latitude=evla[i],longitude=evlo[i],maxradius=0.5) #Get event in order to get more accurate event times.
+            print("i tried. there are this many events", len(cat))
+            if len(cat) > 1:
+                print("WARNING: MORE THAN ONE EVENT OCCURS WITHIN 5km Search!!")
+
+            start.microsecond = cat[0].origins[0].time.microsecond
+            start.second = cat[0].origins[0].time.second
+            start.minute = cat[0].origins[0].time.minute
+        except FDSNNoDataException:
+            print("No Event Data Available")
+        except FDSNException:
+            print("FDSNException for get_events")
+        channel = ["BHN","BHZ","BHE"]
+        for ch in channel:
+            id_tst = "NEW_"  +  str(i) + "_" + ch + ".sac"
+
+            print("Looking for :", id_tst)
+            if os.path.isfile(id_tst) == True:
+                print("It exists. It was not downloaded") # File does not exist
+
+            else:
+                print("It does not exist. Download attempted")
+
+
+                st = obspy.core.stream.Stream() # Initialises our stream variable
+
+                client = Client("IRIS")
+
+                #print("Event: ", i, ". Station: ", station, ". Channel: ", ch)
+
+                test_00 = obspy.core.UTCDateTime("2011-05-02T19:07:00") # This Criterion is ONLY for AAM
+                if start >= test_00:
+                 # Location "00" for AAM went live at 2011/05/02 (122) 19:07:00
+                 # Location "10" is for NEW
+                   loc = "10"
+                   # channel = "BH?"
+                elif start < obspy.core.UTCDateTime("2000-01-01T00:00:00"):
+                    loc = "--"
+                    # channel = "BH?"
+                else:
+                    loc = "--"
+                    # channel = "BH?"
+                try:
+                    st = client.get_waveforms("US",station,loc,ch,start,start + 3000,attach_response=True)
+
+                    if len(st) > 3:
+                        print("WARNING: More than three traces downloaded for event ", i)
+
+                    if ((st[0].stats.endtime - st[0].stats.starttime) >= 2999.0):
+
+                        t = obspy.core.UTCDateTime(st[0].stats.starttime, precision = 0)
+                        yr = str(t.year)
+                        mon = str(t.month).zfill(2) # Converts Month to string. zfill ensures it is 2 characters by adding a leading zero if required
+                        day = str(t.day).zfill(2)   # Day of event, this is to contruct expected
+                        hour = str(t.hour).zfill(2) # Converts Hour to string. zfill ensures it is 2 characters by adding a leading zero if required
+                        minute = str(t.minute).zfill(2)
+
+                        tr_id = stat + "_" + str(i) +"_" + ch + ".sac"
+
+
+                        st[0].write('holder.sac', format='SAC') # Writes traces as SAC files
+                        st_2 = obspy.core.read('holder.sac')
+
+                        #sac = AttribDict() # Creates a dictionary sacd to contain all the header information I want.
+                        ## Station Paramters
+                        st_2[0].stats.sac.stla = stla
+                        st_2[0].stats.sac.stlo = stlo
+                        ## Event Paramters
+                        st_2[0].stats.sac.evla = cat[0].origins[0].latitude # Event latitude
+                        st_2[0].stats.sac.evlo = cat[0].origins[0].longitude # Event longitude
+                        st_2[0].stats.sac.evdp = cat[0].origins[0].depth/1000 # Event depth
+                        dist_client = iris.client.Client() # Creates client to calculate event - station distance
+                        d = dist_client.distaz(stalat=stla,stalon=stlo,evtlat=evla[i],evtlon=evlo[i])
+                        st_2[0].stats.sac.gcarc = list(d.values())[1] # d.values returns the values from dictionary d produced by distaz. list converts this to a list attribute which can then be indexed to extract the great cricle distance in degrees
+                        st_2[0].stats.sac.dist = list(d.values())[2]/1000 # Distnace in kilometers
+
+                        ## File Type
+                        #sacd.iftype = 1
+
+                        #print(st_2[0].stats)
+                        #st[0].stats.sac = sacd
+                        st_2[0].write(tr_id, format='SAC')
+                        print("The trace ", tr_id, "was downloaded and saved!")
+
+                    else:
+                        #print("Trace is not complete so it was not saved.")
+                        too_short = too_short+1
+                except FDSNException:
+                    #print("No data available for event", i+1, ":-(")
+                    no_dat = no_dat + 1 #Counter for number of events where no data was available
+
+
+    #print("Download attempts finished. No data was available for ",no_dat, "of", no_events,"events. ",too_short, "events had incomplete traces which were not saved")
+############## End of Function to Request and Save Traces ####################
+
+
+### Start of Main script ####
+data = pd.read_csv("/Users/ja17375/Scripts/Python/SKS_Splitting/Jacks_SKS_RAW.txt",delim_whitespace=True)
+num_splits = data[data.AUTOQC == 'split' ]['STAT'].value_counts()
+stat=num_splits.index[0] ## When made into a function this will be the requested variable (or one of them at least)
+split1 = data[(data['STAT'] == stat) & (data['AUTOQC'] =="split") ]
+
+### This appear to be the best way to parse the data that we want
+split1 = split1.reset_index()
+del split1['index'] # Resets the indexing of the rows to make them easier to find
+no_events = split1.shape[0]
+date = split1.DATE
+time = split1.TIME
+dist = split1.DIST
+depth = split1.EVDP
+evla = split1.EVLA
+evlo = split1.EVLO
+stla = split1.STLA[0]
+stlo = split1.STLO[0]
+
+# for i in range(0 , split1.shape[0]): # split1.shape[0] returns the number of rows in split1
+# Call the function trace_download, to download traces
+trace_download(date,time,evla,evlo,stla,stlo,stat, no_events)
+
+
+
+
+#Generates the SKS arrival time based upon the listed epcentral distance (DIST) and event depth (EVDP) using the model iasp91
