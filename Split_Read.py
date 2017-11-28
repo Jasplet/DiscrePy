aa6d6c6871d2763697642408573e6dc55441b177
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2017-11-28 16:00:41 +0000

    Some more fixes made. Script now in reasonable working order

--- Split_Read.py
+++ Split_Read.py
@@ -23,7 +23,7 @@ def trace_download(dates,times,evla,evlo,stla,stlo,station, n_events):
     ## Function to download and save traces for a pre-determined set of events
     too_short = 0
     no_dat = 0
-    for i in range(0,n_events):
+    for i in range(0 n_events):
         # Test if trace is already downloaded and create a UTCDateTime object for the Starttime
         ps = str(dates[i]) + "T" + str(times[i]).zfill(4)
         start = obspy.core.UTCDateTime(ps) #iso8601=True
@@ -50,21 +50,14 @@ def trace_download(dates,times,evla,evlo,stla,stlo,station, n_events):
         channel = ["BHN","BHZ","BHE"]
         for ch in channel:
             id_tst = "NEW_"  +  str(i) + "_" + ch + ".sac"
-
             print("Looking for :", id_tst)
             if os.path.isfile(id_tst) == True:
                 print("It exists. It was not downloaded") # File does not exist
-
             else:
                 print("It does not exist. Download attempted")
-
-
                 st = obspy.core.stream.Stream() # Initialises our stream variable
-
                 client = Client("IRIS")
-
                 #print("Event: ", i, ". Station: ", station, ". Channel: ", ch)
-
                 test_00 = obspy.core.UTCDateTime("2011-05-02T19:07:00") # This Criterion is ONLY for AAM
                 if start >= test_00:
                  # Location "00" for AAM went live at 2011/05/02 (122) 19:07:00
@@ -79,25 +72,19 @@ def trace_download(dates,times,evla,evlo,stla,stlo,station, n_events):
                     # channel = "BH?"
                 try:
                     st = client.get_waveforms("US",station,loc,ch,start,start + 3000,attach_response=True)
-
                     if len(st) > 3:
                         print("WARNING: More than three traces downloaded for event ", i)
-
                     if ((st[0].stats.endtime - st[0].stats.starttime) >= 2999.0):
-
                         t = obspy.core.UTCDateTime(st[0].stats.starttime, precision = 0)
                         yr = str(t.year)
                         mon = str(t.month).zfill(2) # Converts Month to string. zfill ensures it is 2 characters by adding a leading zero if required
                         day = str(t.day).zfill(2)   # Day of event, this is to contruct expected
                         hour = str(t.hour).zfill(2) # Converts Hour to string. zfill ensures it is 2 characters by adding a leading zero if required
                         minute = str(t.minute).zfill(2)
-
-                        tr_id = "/Users/ja17375/Scripts/Python/SKS_Splitting/Data" + /stat + "_" + str(i) +"_" + ch + ".sac"
-
-
+                        tr_id = "./Data/" + stat + "_" + str(i) +"_" + ch + ".sac"
                         st[0].write('holder.sac', format='SAC') # Writes traces as SAC files
+                        #st.plot()
                         st_2 = obspy.core.read('holder.sac')
-
                         #sac = AttribDict() # Creates a dictionary sacd to contain all the header information I want.
                         ## Station Paramters
                         st_2[0].stats.sac.stla = stla
@@ -110,29 +97,22 @@ def trace_download(dates,times,evla,evlo,stla,stlo,station, n_events):
                         d = dist_client.distaz(stalat=stla,stalon=stlo,evtlat=evla[i],evtlon=evlo[i])
                         st_2[0].stats.sac.gcarc = list(d.values())[1] # d.values returns the values from dictionary d produced by distaz. list converts this to a list attribute which can then be indexed to extract the great cricle distance in degrees
                         st_2[0].stats.sac.dist = list(d.values())[2]/1000 # Distnace in kilometers
-
                         ## File Type
                         #sacd.iftype = 1
-
                         #print(st_2[0].stats)
-                        #st[0].stats.sac = sacd
+                        #st[0].stats.sac = sac
+                        #st_2.plot()
                         st_2[0].write(tr_id, format='SAC')
                         print("The trace ", tr_id, "was downloaded and saved!")
-
                     else:
                         #print("Trace is not complete so it was not saved.")
                         too_short = too_short+1
                 except FDSNException:
                     #print("No data available for event", i+1, ":-(")
-                    no_dat = no_dat + 1 #Counter for number of events where no data was available
-
-
-    #print("Download attempts finished. No data was available for ",no_dat, "of", no_events,"events. ",too_short, "events had incomplete traces which were not saved")
+                    no_dat = no_dat + 1 #Counter for number of events where no data was availables
 ############## End of Function to Request and Save Traces ####################
-
-
-### Start of Main script ####
-data = pd.read_csv("/Users/ja17375/Scripts/Python/SKS_Splitting/Jacks_SKS_RAW.txt",delim_whitespace=True)
+############## Start of Main script ##########################################
+data = pd.read_csv("./Data/Jacks_SKS_RAW.txt",delim_whitespace=True)
 num_splits = data[data.AUTOQC == 'split' ]['STAT'].value_counts()
 stat=num_splits.index[0] ## When made into a function this will be the requested variable (or one of them at least)
 split1 = data[(data['STAT'] == stat) & (data['AUTOQC'] =="split") ]
@@ -153,8 +133,3 @@ stlo = split1.STLO[0]
 # for i in range(0 , split1.shape[0]): # split1.shape[0] returns the number of rows in split1
 # Call the function trace_download, to download traces
 trace_download(date,time,evla,evlo,stla,stlo,stat, no_events)
-
-
-
-
-#Generates the SKS arrival time based upon the listed epcentral distance (DIST) and event depth (EVDP) using the model iasp91
