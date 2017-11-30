d4f88b73ffc1bd1961fc31f78878a98b2c969386
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2017-11-30 17:10:07 +0000

    Updates finished. Script can now be used to measure and recorded SKS splitting. Potential for further tidying up and converting to using a class instead of global variables in the future

--- Split_Measure.py
+++ Split_Measure.py
@@ -11,7 +11,7 @@ import os.path
 import splitwavepy as sw
 from obspy.clients.fdsn import Client
 from obspy.taup import TauPyModel
-
+import matplotlib.gridspec as gridspec
 #### Reading downloaded traces and Windowing data
 # Generate Travel time model for SKS phase for all events prior to Windowing
 
@@ -41,7 +41,7 @@ def model_SKS(tr):
     SKS = model.get_travel_times(tr.stats.sac.evdp,tr.stats.sac.gcarc,["SKS"])[0].time
     t0 = st[0].stats.starttime # Start time of stream for event. This should be the event origin time.
     UTC = obspy.core.utcdatetime.UTCDateTime(t0 + SKS)# SKS arrival time relative to trace start as a UTCDateTime object
-    return SKS,t0,UTC
+    return UTC, t0
     #def Split_Measure(no_events):
 
 def st_prep(st,trim,f_min,f_max,SKS):
@@ -52,55 +52,136 @@ def st_prep(st,trim,f_min,f_max,SKS):
     st.trim(starttime = SKS_UTC - trim, endtime = SKS_UTC + trim)
     return sw.Pair(st[0].data,st[1].data,delta = st[0].stats.delta)
 
+def eigen_plot(eign,fig,**kwargs):
+
+    # setup figure and subplots
+    #fig = plt.figure(figsize=(12,6))
+    gs = gridspec.GridSpec(2, 3,width_ratios=[1,1,2])
+    ax0 = plt.subplot(gs[0,0])
+    ax1 = plt.subplot(gs[0,1])
+    ax2 = plt.subplot(gs[1,0])
+    ax3 = plt.subplot(gs[1,1])
+    ax4 = plt.subplot(gs[:,2])
+
+    # data to plot
+    d1 = eign.data.chop()
+    d1f = eign.srcpoldata().chop()
+    d2 = eign.data_corr().chop()
+    d2s = eign.srcpoldata_corr().chop()
+
+    # flip polarity of slow wave in panel one if opposite to fast
+    # d1f.y = d1f.y * np.sign(np.tan(eign.srcpol()-eign.fast))
+
+    # get axis scaling
+    lim = np.abs(d2s.data()).max() * 1.1
+    ylim = [-lim,lim]
+
+    # original
+    d1f._ptr(ax0,ylim=ylim,**kwargs)
+    d1._ppm(ax1,lims=ylim,**kwargs)
+    # corrected
+    d2s._ptr(ax2,ylim=ylim,**kwargs)
+    d2._ppm(ax3,lims=ylim,**kwargs)
+
+    # error surface
+    if 'vals' not in kwargs:
+        # kwargs['vals'] = (eign.lam1 - eign.lam2) / eign.lam2
+        # kwargs['title'] = r'$(\lambda_1 - \lambda_2) / \lambda_2$'
+        kwargs['vals'] = eign.lam1 / eign.lam2
+        kwargs['title'] = r'$\lambda_1 / \lambda_2$'
+
+    # add marker and info box by default
+    if 'marker' not in kwargs: kwargs['marker'] = True
+    if 'info' not in kwargs: kwargs['info'] = True
+    if 'conf95' not in kwargs: kwargs['conf95'] = True
+    eign._psurf(ax4,**kwargs)
+    # neaten
+    plt.tight_layout()
+
 def measure(pair):
     """
     Function for Picking the window for a provded pair object and then measure the splitting
     """
     pair.plot(pick=True,marker = 150) # Plots the window picker.
     split = sw.EigenM(pair,lags=(4,) )
-    figure = plt.figure()
-    split.plot(fig = figure)
 
-    cid = figure.canvas.mpl_connect('key_press_event',quality)
-    plt.show()
+    fig = plt.figure(figsize=(12,6))
+    eigen_plot(split,fig)
 
+    cid = fig.canvas.mpl_connect('key_press_event',interact)
+    plt.show(fig)
+    fig.canvas.mpl_disconnect(cid)
+    return split, pair.wbeg, pair.wend
 
-def quality(event):
+def null():
+    var = input("Null Measurement (y/n)")
+    if var is 'y':
+        quality.append('null')
+        print("Null Confirmed")
+        plt.close()
+    elif var is 'n':
+        print("Ok, repeating measurement")
+        measure(pair_glob)
+    else:
+        print("Invalid response, try again")
+        null()
+
+def interact(event):
     if event.key in ['a','b','c']:
-        quality = event.key
-    elif event.key == 'x':
-        print('Call function to remeasure')
+        quality.append(event.key)
+        plt.close()
+    elif event.key is 'n':
+        null()
+    elif event.key is 'x':
+        quality.append('x')
+        plt.close()
+    elif event.key == 'r':
+        plt.close()
+        measure(pair_glob)
+
+
 ###################################################
 
-output_file = open('NEW_Splitting.txt','w')
-output_file.write('ID YEAR MON DAY HOUR MIN SEC STAT FAST DFAST TLAG DTLAG WBEG WEND \n')
 
-for i in range(100,101):
+
+
+for i in range(0,101):
     st_id = "NEW_" + str(i).zfill(2) + "_" + "*.sac" # Generate expected file names. Wildcard used to catch all 3 channels
-    # filename = "./Splitting/NEW_" + str(i).zfill(2) + ".eigm"
+    filename = "./Splitting/NEW_" + str(i).zfill(2) + ".eigm"
+    output_file = open('NEW_Splitting.txt','w')
+    output_file.write('ID YEAR MON DAY HOUR MIN SEC STAT FAST DFAST TLAG DTLAG WBEG WEND \n')
+
     st = read_sac(st_id)
 
-    if st != False:
-        #Traces for event have been succesfully read so lets try to measure splittiing!
-        (SKS, origin, SKS_UTC) = model_SKS(st[0])
+    if st != False: #i.e. if the stream is sufficiently populated and has been read.
+
+        SKS_UTC, t0 = model_SKS(st[0])
+        # Intialise some global variables which I need to pass things between fucntions (this is probably not be best way to do things but it works!)
+        global pair_glob
+        global quality
+
+        quality = []
         pair = st_prep(st = st,trim = 150, f_min = 0.01,f_max = 0.5, SKS = SKS_UTC)
-        measure(pair)
+        pair_glob = pair
+        split, wbeg, wend = measure(pair)
+
 
 
         ## Callback key entries for estimated quality of splitting measurements
-            #if key is (A,B,C):
-            #   split.save(filename) # Saves splitting measurements
-    #     window1 = split_pair.wbeg()
-    #     window2 = split_pair.wend()
-    #     date = str(t0.year)+" "+ str(t0.month).zfill(2)+" "+str(t0.day).zfill(2) +" " # Format date information so that is inteligible when output
-    #     t = str(t0.hour).zfill(2)+" "+str(t0.minute).zfill(2)+" "+str(t0.second).zfill(2) # Formating time infomation for printing
-    #     row = str(i)+' '+date+' '+t+' NEW '+str(split.fast)+' '+str(split.dfast)+' '+str(split.lag)+' '+str(split.dlag)+ ' '+ str(window1) + ' '+ str(window2) +'\n' #Row of data to be written to output textfile
-    #         #elif key is X:
-    #             #window_trace(pair, 150)
-    #             #measure splitting again
-    #
-    # row = str(i)+' N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A \n'
-    # output_file.write(row)
+        if quality is not ('x'):
+            split.save(filename) # Saves splitting measurements
+
+            date = str(t0.year)+" "+ str(t0.month).zfill(2)+" "+str(t0.day).zfill(2) +" " # Format date information so that is inteligible when output
+            t = str(t0.hour).zfill(2)+" "+str(t0.minute).zfill(2)+" "+str(t0.second).zfill(2) # Formating time infomation for printing
+            meas = str(split.fast)+' '+str(split.dfast)+' '+str(split.lag)+' '+str(split.dlag)+' '+ str(wbeg())+' '+str(wend())+' '
+            row = str(i)+' '+date+' '+t+' NEW '+ meas + str(quality)+ '\n' #Row of data to be written to output textfile
+        else:
+            row = str(i)+' N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A Bad\n'
+
+    else:
+        row = str(i)+' N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A N/A No Stream\n'
+        print('No stream for event',i)
+        output_file.write(row)
 
 output_file.close()
 
