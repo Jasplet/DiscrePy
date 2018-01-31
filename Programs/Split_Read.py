9cd8c31c21edac45f690b8de7dc9df29d23c807c
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-01-31 10:31:04 +0000

    Changed some behaviours and added counters for successful/failed downloads. Download function now takes information for a single event allowing for better flexibility in the future. Also the output summary textfile {Station}_downloaded_streams is rewritten every time the function is called. Events which are skipped due to being already downloaded are also added to this textfile ensuring a complete record

--- Programs/Split_Read.py
+++ Programs/Split_Read.py
@@ -19,139 +19,120 @@ from obspy.clients import iris
 
 
 ##################### Start of Function to Request and Save Traces ##################
-def trace_download(dates,times,evla,evlo,evdp,stla,stlo,station,network,type, n_events):
-    ## Function to download and save traces for a pre-determined set of events
-
-    too_short = 0
-    no_dat = 0
-    outfile = open('/Users/ja17375/Python/SKS_Splitting/Data/{}/{}_downloaded_{}_streams.txt'.format(station,station,type),'w+')
-    for i in range(0, n_events):
-        # Test if trace is already downloaded and create a UTCDateTime object for the Starttime
-        ps = str(dates[i]) + "T" + str(times[i]).zfill(4)
-        start = obspy.core.UTCDateTime(ps) #iso8601=True
-        # client = obspy.clients.fdsn.Client("IRIS")
-        n = 0
-        client = obspy.clients.fdsn.Client('IRIS') # BKS Station data is hosted by NCEDC so we need this as the client
-        try:
-            cat = client.get_events(starttime=start-60,endtime=start+60 ,latitude=evla[i],longitude=evlo[i],maxradius=0.5) #Get event in order to get more accurate event times.
-            if len(cat) > 1:
-                print("WARNING: MORE THAN ONE EVENT OCCURS WITHIN 5km Search!!")
-
-            start.microsecond = cat[0].origins[0].time.microsecond
-            start.second = cat[0].origins[0].time.second
-            start.minute = cat[0].origins[0].time.minute
-            sec = str(start.second).zfill(2)
-            n = 1
-        except FDSNNoDataException:
-            print("No Event Data Available")
-        except FDSNException:
-            print("FDSNException for get_events")
-
-        channel = ["BHN","BHZ","BHE"]
-
-        date = '{:07d}'.format(dates[i])
-        if n == 0:
-            time = '{:04d}00'.format(times[i])
-        elif n ==1:
-            time = '{:04d}{}'.format(times[i],sec)
-
-        for ch in channel:
-
-            id_tst = "/Users/ja17375/Python/SKS_Splitting/Data/{}/{}_{}_{}_{}.sac".format(station,station,date,time,ch)
-            print("Looking for :", id_tst)
-            if os.path.isfile(id_tst) == True:
-                print("It exists. It was not downloaded") # File does not exist
-            else:
-                print("It does not exist. Download attempted")
-                st = obspy.core.stream.Stream() # Initialises our stream variable
-
-                loc = '--'
-
-                #print("Event: ", i, ". Station: ", station, ". Channel: ", ch)
-                # test_00 = obspy.core.UTCDateTime("2011-05-02T19:07:00") # This Criterion is ONLY for AAM
-                # if start >= test_00:
-                #  # Location "00" for AAM went live at 2011/05/02 (122) 19:07:00
-                #  # Location "10" is for NEW
-                #    loc = "10"
-                #
-                # elif start < obspy.core.UTCDateTime("2000-01-01T00:00:00"):
-                #     loc = "--"
-                #
-                # else:
-                #     loc = "--"
-
-                try:
-
-                    download_client = obspy.clients.fdsn.Client('IRIS')
-                    st = download_client.get_waveforms(network,station,'??',ch,start,start + 3000,attach_response=True)
-
-                    if len(st) > 3:
-                        print("WARNING: More than three traces downloaded for event ", i)
-                    if ((st[0].stats.endtime - st[0].stats.starttime) >= 2999.0):
-
-                        tr_id = "/Users/ja17375/Python/SKS_Splitting/Data/{}/{}_{}_{}_{}.sac".format(station,station,date,time,ch)
-                        st[0].write('holder.sac', format='SAC',) # Writes traces as SAC files
-                        #st.plot()
-                        st_2 = obspy.core.read('holder.sac')
-                        #sac = AttribDict() # Creates a dictionary sacd to contain all the header information I want.
-                        ## Station Paramters
-                        st_2[0].stats.sac.stla = stla
-                        st_2[0].stats.sac.stlo = stlo
-                        ## Event Paramters
-                        st_2[0].stats.sac.evla = evla[i]#cat[0].origins[0].latitude # Event latitude
-                        st_2[0].stats.sac.evlo = evlo[i]#cat[0].origins[0].longitude # Event longitude
-                        st_2[0].stats.sac.evdp = evdp[i]#cat[0].origins[0].depth/1000 # Event depth
-                        dist_client = iris.client.Client() # Creates client to calculate event - station distance
-                        d = dist_client.distaz(stalat=stla,stalon=stlo,evtlat=evla[i],evtlon=evlo[i])
-                        st_2[0].stats.sac.gcarc = d['distance'] # d.values returns the values from dictionary d produced by distaz. list converts this to a list attribute which can then be indexed to extract the great cricle distance in degrees
-                        st_2[0].stats.sac.dist = d['distancemeters']/1000 # Distnace in kilometers
-                        st_2[0].stats.sac.baz = d['backazimuth'] # Backzimuth (Reciever - SOurce)
-                        st_2[0].stats.sac.az = d['azimuth'] # Azimuth (Source - Receiver)
-                        ## File Type
-                        #sacd.iftype = 1
-                        #print(st_2[0].stats)
-                        #st[0].stats.sac = sac
-                        #st_2.plot()
-                        st_2[0].write(tr_id, format='SAC')
-                        print("The trace ", tr_id, "was downloaded and saved!")
-
-                        if ch == 'BHE':
-                            outfile.write('{}\n'.format(tr_id[0:-7]))
-
-
-                    else:
-                        print("Trace is too short.")
-                        too_short = too_short+1
-                except FDSNException:
-                    #print("No data available for event", i+1, ":-(")
-                    no_dat = no_dat + 1 #Counter for number of events where no data was availables
-
-def split_read(station,network,autoqc):
+def split_read(station,network):
     """
     Initialises some variable and call the trace_dowload function for a given station
     """
-    data = pd.read_csv('/Users/ja17375/Python/SKS_Splitting/Data/Jacks_SKS_RAW.txt',delim_whitespace=True,keep_default_na=False)
-    num_splits = data[data.AUTOQC == 'split' ]['STAT'].value_counts()
-    # stat=num_splits.index[0] ## When made into a function this will be the requested variable (or one of them at least)
+    raw = pd.read_csv('/Users/ja17375/Python/SKS_Splitting/Data/Jacks_SKS_RAW.txt',delim_whitespace=True,keep_default_na=False)
 
-    split1 = data[(data['STAT'] == station) & (data['AUTOQC'] == autoqc)]
-    split1 = split1.reset_index()
-    del split1['index']
+    data = raw[(raw['STAT'] == station) & (raw['AUTOQC'] != 'fail')]
     ### This appear to be the best way to parse the data that we want
     # Resets the indexing of the rows to make them easier to find
+    data = data.reset_index()
+    del data['index']
 
 
-    date = split1.DATE
-    time = split1.TIME
-    dist = split1.DIST
-    evdp = split1.EVDP
-    evla = split1.EVLA
-    evlo = split1.EVLO
-    stla = split1.STLA[0]
-    stlo = split1.STLO[0]
+    outfile = open('/Users/ja17375/Python/SKS_Splitting/Data/{}/{}_downloaded_streams.txt'.format(station,station),'w+')
 
-    no_events = len(date) # Each event has a date, so the length of date gives the number of events
+    attempts = 0 #Counter for how many attempted downloads there were
+    fdsnx = 0 #Counter for how many attempts hit a FDSNNoDataException
+    dwn = 0 #Counter for how many events were downloaded
+    ex = 0 #Counter for how many event already exist in filesystem and therefore werent downloaded
+    ts = 0
 
-    # for i in range(0 , split1.shape[0]): # split1.shape[0] returns the number of rows in split1
+    for i in range(0,len(data)):
     # Call the function trace_download, to download traces
-    trace_download(date,time,evla,evlo,evdp,stla,stlo,station,network,autoqc, no_events)
+        dwn,fdsnx, ex, ts = trace_download(data.DATE[i],data.TIME[i],data.EVLA[i],data.EVLO[i],data.EVDP[i],data.STLA[i],data.STLO[i],station,network,outfile,fdsnx,ex,dwn,ts)
+        attempts += 1
+
+    print('{:03d} download attempts were made, {:02d} were successful, {:02d} hit FDSNNoDataExceptions, {:02} were incomplete and {:02d} aboarted as the data has already been downloaded'.format(attempts,dwn,fdsnx,ts,ex))
+
+def trace_download(date,time,evla,evlo,evdp,stla,stlo,station,network,outfile,fdsnx,ex,dwn,ts):
+    ## Function to download and save traces for a pre-determined set of events
+
+
+    datetime = str(date) + "T" + str(time).zfill(4) #Combined date and time inputs for converstion t UTCDateTime object
+    start = obspy.core.UTCDateTime(datetime) #iso8601=True
+
+    client = obspy.clients.fdsn.Client('IRIS') #
+    try:
+        cat = client.get_events(starttime=start-60,endtime=start+60 ,latitude=evla,longitude=evlo,maxradius=0.5) #Get event in order to get more accurate event times.
+        if len(cat) > 1:
+            print("WARNING: MORE THAN ONE EVENT OCCURS WITHIN 5km Search!!")
+
+        start.second = cat[0].origins[0].time.second
+
+        if start.minute != cat[0].origins[0].time.minute:
+            time = (time - start.minute) + cat[0].origins[0].time.minute # Time is hhmm so we subtract the old minute value and add the new one
+
+    except FDSNNoDataException:
+        print("No Event Data Available")
+    except FDSNException:
+        print("FDSNException for get_events")
+
+
+    channel = ["BHN","BHZ","BHE"]
+    for ch in channel:
+
+        tr_id = "/Users/ja17375/Python/SKS_Splitting/Data/{}/{}_{:07d}_{:04d}{:02d}_{}.sac".format(station,station,date,time,start.second,ch)
+        # print("Looking for :", id_tst)
+        if os.path.isfile(tr_id) == True:
+            print("It exists. It was not downloaded") # File does not exist
+            if ch == 'BHE':
+                outfile.write('{}\n'.format(tr_id[0:-7]))
+                ex += 1
+        else:
+            # print("It does not exist. Download attempted")
+            st = obspy.core.stream.Stream() # Initialises our stream variable
+            try:
+
+                download_client = obspy.clients.fdsn.Client('NCEDC')
+                st = download_client.get_waveforms(network,station,'??',ch,start,start + 3000,attach_response=True)
+
+                if len(st) > 3:
+                    print("WARNING: More than three traces downloaded for event ", tr_id)
+                if ((st[0].stats.endtime - st[0].stats.starttime) >= 2999.0):
+
+
+                    st[0].write('holder.sac', format='SAC',) # Writes traces as SAC files
+                    #st.plot()
+                    st_2 = obspy.core.read('holder.sac')
+                    #sac = AttribDict() # Creates a dictionary sacd to contain all the header information I want.
+                    ## Station Paramters
+                    st_2[0].stats.sac.stla = stla
+                    st_2[0].stats.sac.stlo = stlo
+                    ## Event Paramters
+                    st_2[0].stats.sac.evla = evla#cat[0].origins[0].latitude # Event latitude
+                    st_2[0].stats.sac.evlo = evlo#cat[0].origins[0].longitude # Event longitude
+                    st_2[0].stats.sac.evdp = evdp#cat[0].origins[0].depth/1000 # Event depth
+                    dist_client = iris.Client() # Creates client to calculate event - station distance
+                    print('stla = {}, stlo = {}, evla = {}, evlo = {}'.format(stla,stlo,evla,evlo))
+
+                    d = dist_client.distaz(stalat=stla,stalon=stlo,evtlat=evla,evtlon=evlo)
+
+                    st_2[0].stats.sac.gcarc = d['distance'] # d.values returns the values from dictionary d produced by distaz. list converts this to a list attribute which can then be indexed to extract the great cricle distance in degrees
+                    st_2[0].stats.sac.dist = d['distancemeters']/1000 # Distnace in kilometers
+                    st_2[0].stats.sac.baz = d['backazimuth'] # Backzimuth (Reciever - SOurce)
+                    st_2[0].stats.sac.az = d['azimuth'] # Azimuth (Source - Receiver)
+
+
+                    ## File Type
+                    #sacd.iftype = 1
+                    #print(st_2[0].stats)
+                    #st[0].stats.sac = sac
+                    #st_2.plot()
+                    st_2[0].write(tr_id, format='SAC')
+                    print("The trace ", tr_id, "was downloaded and saved!")
+                    dwn += 1
+                    if ch == 'BHE':
+                        outfile.write('{}\n'.format(tr_id[0:-7]))
+
+
+                else:
+                    print("Trace is too short.")
+                    ts += 1
+            except FDSNException:
+                if ch == 'BHE':
+                    fdsnx += 1
+
+    return dwn, fdsnx, ex,ts
