a24f91319b8908497f848056956b12f0ee78c758
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-05 18:31:58 +0000

    Changed subplot style. Fixed and confirmed working !

--- Programs/plot.py
+++ Programs/plot.py
@@ -18,39 +18,36 @@ def SKS_plot(file,title1):
     data = data[(data.QUAL != 'x')]
     fig,axs = plt.subplots(2, 2,sharex='col',figsize=(10,10))
 
-    ax1 = subplot(221)
-    ax1.errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].FAST,yerr=data[(data.QUAL == 'n')].DFAST,fmt='kx',elinewidth=0.5,label='Null')
-    ax1.errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].FAST,yerr=data[(data.QUAL != 'n')].DFAST,fmt='ko',elinewidth=0.5,label='Split')
-    ax1.legend(loc=2)
 
-    ax1.ylabel('Fast Direction (deg)')
-    ax1.ylim([-90,90])
-    ax1.yticks(np.arange(-90,91,30))
-    ax1.title('{} - Fast Direction'.format(title1))
+    axs[0,0].errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].FAST,yerr=data[(data.QUAL == 'n')].DFAST,fmt='kx',elinewidth=0.5,label='Null')
+    axs[0,0].errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].FAST,yerr=data[(data.QUAL != 'n')].DFAST,fmt='ko',elinewidth=0.5,label='Split')
+    axs[0,0].legend(loc=2)
 
-    ax2 = subplot(223)
-    ax2.errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].WL_FAST,yerr=data[(data.QUAL == 'n')].WL_DFAST,fmt='kx',elinewidth=0.5)
-    ax2.errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].WL_FAST,yerr=data[(data.QUAL != 'n')].WL_DFAST,fmt='ko',elinewidth=0.5)
-    ax2.ylim([-90,90])
-    ax2.yticks(np.arange(-90,91,30))
-    ax2.title('Jacks(Sheba) - Fast Direction')
-    ax2.xlabel('Back Azimuth')
-    ax2.ylabel('Fast Direction (deg)')
+    axs[0,0].set_ylabel('Fast Direction (deg)')
+    axs[0,0].set_ylim([-90,90])
+    axs[0,0].set_yticks(np.arange(-90,91,30))
+    axs[0,0].set_title('{} - Fast Direction'.format(title1))
 
-    ax3 = subplot(222)
-    ax3.errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].TLAG,yerr=data[(data.QUAL == 'n')].DTLAG,fmt='kx',elinewidth=0.5)
-    ax3.errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].TLAG,yerr=data[(data.QUAL != 'n')].DTLAG,fmt='ko',elinewidth=0.5)
-    ax3.ylabel('Tlag (s)')
-    ax3.ylim([0,4])
-    ax3.title('{} - Lag Time'.format(title1))
+    axs[0,1].errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].WL_FAST,yerr=data[(data.QUAL == 'n')].WL_DFAST,fmt='kx',elinewidth=0.5)
+    axs[0,1].errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].WL_FAST,yerr=data[(data.QUAL != 'n')].WL_DFAST,fmt='ko',elinewidth=0.5)
+    axs[0,1].set_ylim([-90,90])
+    axs[0,1].set_yticks(np.arange(-90,91,30))
+    axs[0,1].set_title('Jacks(Sheba) - Fast Direction')
+    axs[0,1].set_xlabel('Back Azimuth')
+    axs[0,1].set_ylabel('Fast Direction (deg)')
 
-    ax4 = subplot(224)
-    ax4.errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].WL_TLAG,yerr=data[(data.QUAL == 'n')].WL_DTLAG,fmt='kx',elinewidth=0.5)
-    ax4.errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].WL_TLAG,yerr=data[(data.QUAL != 'n')].WL_DTLAG,fmt='ko',elinewidth=0.5)
-    ax4.ylim([0,4])
-    ax4.ylabel('Tlag (s)')
-    ax4.xlabel('Back Azimuth')
-    ax4.title('Jacks(Sheba) - Lag Time')
+    axs[1,0].errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].TLAG,yerr=data[(data.QUAL == 'n')].DTLAG,fmt='kx',elinewidth=0.5)
+    axs[1,0].errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].TLAG,yerr=data[(data.QUAL != 'n')].DTLAG,fmt='ko',elinewidth=0.5)
+    axs[1,0].set_ylabel('Tlag (s)')
+    axs[1,0].set_ylim([0,4])
+    axs[1,0].set_title('{} - Lag Time'.format(title1))
+
+    axs[1,1].errorbar(data[(data.QUAL == 'n')].BAZ,data[(data.QUAL == 'n')].WL_TLAG,yerr=data[(data.QUAL == 'n')].WL_DTLAG,fmt='kx',elinewidth=0.5)
+    axs[1,1].errorbar(data[(data.QUAL != 'n')].BAZ,data[(data.QUAL != 'n')].WL_TLAG,yerr=data[(data.QUAL != 'n')].WL_DTLAG,fmt='ko',elinewidth=0.5)
+    axs[1,1].set_ylim([0,4])
+    axs[1,1].set_ylabel('Tlag (s)')
+    axs[1,1].set_xlabel('Back Azimuth')
+    axs[1,1].set_title('Jacks(Sheba) - Lag Time')
 
 
     plt.tight_layout()
@@ -70,7 +67,7 @@ def _fast(ax,baz,fast1,dfast1):
     ax.ylabel('Lag (s)')
     ax.xlabel('Back Azimuth (deg)')
 
-def coverage(evla,evla,stla,stlo,stat):
+def coverage(evla,evlo,stla,stlo,stat):
     fig = plt.figure(figsize = (10,10))
     # We can either do and AzimuthalEquidistant projection centered on the staiton or a nice, Pacific-centered one
     # proj = cart.PlateCarree(central_longitude=180)
