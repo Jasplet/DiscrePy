a95994a906853de471cebc36a9df0343f9448a4e
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-05 19:18:42 +0000

    Bodged SKKS_plot so that the 3 seperate plots appear on final image. Seperating plotting into functions does not appear to work

--- Programs/plot.py
+++ Programs/plot.py
@@ -3,7 +3,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import cartopy as cart
+import cartopy.crs as cart
+import cartopy
 import matplotlib.gridspec as gridspec
 import obspy
 
@@ -60,18 +61,44 @@ def SKKS_plot(stat,phase):
     a = data['FAST']
     d = data.index[np.isnan(data['FAST']) == True].tolist() # Find any rows which contain NaNs
     data = data.drop(d)
-    data = data[(data.QUAL != 'x')]`
+    data = data[(data.QUAL != 'x')]
+    fig = plt.figure(figsize = [20,20])
+    axs = []
+    gs = gridspec.GridSpec(2,3)
+    proj = cart.AzimuthalEquidistant(central_longitude=data.STLO[0],central_latitude=data.STLA[0])
 
-    gs = gridspec.GridSpec(2,3):
 
     ax1 = plt.subplot(gs[0,0])
     ax2 = plt.subplot(gs[1,0])
-    ax2 = plt.subplot(gs[:,1:])
+    ax3 = fig.add_subplot(gs[:,1:],projection = proj)
 
+    ax1.errorbar(data.BAZ,data.TLAG,yerr = data.DTLAG,fmt='kx',elinewidth=0.5)
+    ax1.set_ylim([0,4])
+    ax1.set_ylabel('Lag (s)')
+    ax1.set_xlabel('Back Azimuth (deg)')
+    # _lag(ax1,data.BAZ,data.TLAG,data.DTLAG,'kx')
+    # _fast(ax2,data.BAZ,data.FAST,data.DFAST,'kx')
+    # coverage(ax3,data.EVLA,data.EVLO,data.STLA[0],data.STLO[0],stat)
+    # plt.show()
+    ax2.errorbar(data.BAZ,data.FAST,yerr=data.DFAST,fmt='kx',elinewidth=0.5)
+    ax2.set_ylim([-90,90])
+    ax2.set_ylabel('Fast Direction (s)')
+    ax2.set_xlabel('Back Azimuth (deg)')
+    ### Plot Coverage on third axis object
+
+    ax3.set_global() #This sets the axes extent to its maximum possible setting, so we can find these darn events
+    ax3.coastlines(resolution = '110m') # Coastline data is downloaded by Cartopy from Natural Earth (http://www.naturalearthdata.com)
+    #Resolution options are 100m,50m and 10m
+    ax3.add_feature(cartopy.feature.OCEAN, zorder=0)
+    ax3.add_feature(cartopy.feature.LAND, zorder=0, edgecolor='black')
+    # Now add observed events
+    ax3.plot(data.EVLO,data.EVLA,'ro',markersize = 5,transform = cart.Geodetic(),label='Event Location')
+    # ax.set_xticks([-130,-125,-120,-115,-110,-105,-100], crs=proj)
+    # ax.set_yticks([30,35,40,45,50,55,60], crs=proj)
+    ax3.plot(data.STLO,data.STLA,'kv',transform=cart.Geodetic(),markersize=10,label='Station Loction')
+    ax3.set_title('Coverage for Station {}'.format(stat))
+    ax3.legend()
     plt.show()
-
-
-
 
 def _lag(ax,baz,lag1,dlag1,fmt):
 
@@ -81,15 +108,15 @@ def _lag(ax,baz,lag1,dlag1,fmt):
     ax.set_ylabel('Lag (s)')
     ax.set_xlabel('Back Azimuth (deg)')
 
-def _fast(ax,baz,fast1,dfast1):
+def _fast(ax,baz,fast1,dfast1,fmt):
 
-    ax.errorbar(baz,lag1,yerr=dlag1,fmt = 'kx',elinewidth=0.5)
+    ax.errorbar(baz,fast1,yerr=dfast1,fmt = fmt,elinewidth=0.5)
     ax.set_ylim([-90,90])
     ax.set_ylabel('Fast Direction (s)')
     ax.set_xlabel('Back Azimuth (deg)')
 
-def coverage(evla,evlo,stla,stlo,stat):
-    fig = plt.figure(figsize = (10,10))
+def coverage(ax,evla,evlo,stla,stlo,stat):
+
     # We can either do and AzimuthalEquidistant projection centered on the staiton or a nice, Pacific-centered one
     # proj = cart.PlateCarree(central_longitude=180)
     proj = cart.AzimuthalEquidistant(central_longitude=stlo,central_latitude=stla)
@@ -109,5 +136,5 @@ def coverage(evla,evlo,stla,stlo,stat):
     # ax.set_yticks([30,35,40,45,50,55,60], crs=proj)
     stat = 'NEW'
     ax.plot(stlo,stla,'kv',transform=cart.Geodetic(),markersize=10,label='Station Loction')
-    plt.title('Coverage for Station {}'.format(stat))
+    ax.set_title('Coverage for Station {}'.format(stat))
     ax.legend()
