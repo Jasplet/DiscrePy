f4d9fc79093d08c817563e79bddfc7a68fea6457
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-06 14:58:11 +0000

    Created new function SKS_SKKS_plot to make a 3 panel plot showing fast,tlag v baz and coverage

--- Programs/plot.py
+++ Programs/plot.py
@@ -19,7 +19,7 @@ def load(stat,phase):
 
     return data
 
-def SKS_plot(stat,title1,phase=SKS):
+def SKS_plot(stat,title1,phase='SKS'):
     """
     Function to make diagnostice plots for a given file of splitting measuremtns
     """
@@ -105,6 +105,58 @@ def SKKS_plot(stat,phase):
     # ax.set_xticks([-130,-125,-120,-115,-110,-105,-100], crs=proj)
     # ax.set_yticks([30,35,40,45,50,55,60], crs=proj)
     ax3.plot(data.STLO,data.STLA,'kv',transform=cart.Geodetic(),markersize=10,label='Station Loction')
+    ax3.set_title('{} coverage for Station {}'.format(phase,stat))
+    ax3.legend()
+    plt.show()
+
+def SKS_SKKS_plot(stat):
+    """
+    Creates a 3 panel plot showing measured fast direction and lag time vs back azimuth at a given station along with the coverage of the events
+    for BOTH SKS  and SKKS
+    stat - station Code [STRING]
+    phase - phase to plot [STRING]
+    """
+    SKS_data = load(stat,phase='SKS')
+    SKKS_data = load(stat,phase='SKKS')
+
+    fig = plt.figure(figsize = [20,20])
+    axs = []
+    gs = gridspec.GridSpec(2,3) # Creates a 2x3 grid in the figure space for plotting in
+    proj = cart.AzimuthalEquidistant(central_longitude=data.STLO[0],central_latitude=data.STLA[0])
+
+
+    ax1 = plt.subplot(gs[0,0])
+    ax2 = plt.subplot(gs[1,0])
+    ax3 = fig.add_subplot(gs[:,1:],projection = proj) # have to use add_subplots in order to add a different projection
+
+    ax1.errorbar(SKS_data.BAZ,SKS_data.TLAG,yerr = SKS_data.DTLAG,fmt='rx',elinewidth=0.5,label='SKS')
+    ax1.errorbar(SKKS_data.BAZ,SKKS_data.TLAG,yerr=SKKS_data.DTLAG,fmt = 'bx',elinewidth=0.5,label='SKKS')
+    ax1.set_ylim([0,4])
+    ax1.set_ylabel('Lag (s)')
+    ax1.set_xlabel('Back Azimuth (deg)')
+    ax1.legend()
+    # _lag(ax1,data.BAZ,data.TLAG,data.DTLAG,'kx')
+    # _fast(ax2,data.BAZ,data.FAST,data.DFAST,'kx')
+    # coverage(ax3,data.EVLA,data.EVLO,data.STLA[0],data.STLO[0],stat)
+    # plt.show()
+    ax2.errorbar(SKS_data.BAZ,SKS_data.FAST,yerr = SKS_data.DFAST,fmt='rx',elinewidth=0.5,label='SKS')
+    ax2.errorbar(SKKS_data.BAZ,SKKS_data.FAST,yerr=SKKS_data.DFAST,fmt = 'bx',elinewidth=0.5,label='SKKS')
+    ax2.set_ylim([-90,90])
+    ax2.set_ylabel('Fast Direction (s)')
+    ax2.set_xlabel('Back Azimuth (deg)')
+    ### Plot Coverage on third axis object
+
+    ax3.set_global() #This sets the axes extent to its maximum possible setting, so we can find these darn events
+    ax3.coastlines(resolution = '110m') # Coastline data is downloaded by Cartopy from Natural Earth (http://www.naturalearthdata.com)
+    #Resolution options are 100m,50m and 10m
+    ax3.add_feature(cartopy.feature.OCEAN, zorder=0)
+    ax3.add_feature(cartopy.feature.LAND, zorder=0, edgecolor='black')
+    # Now add observed events
+    ax3.plot(SKS_data.EVLO,SKS_data.EVLA,'ro',markersize = 5,transform = cart.Geodetic(),label='Event Locations (SKS)')
+    ax3.plot(SKKS_data.EVLO,SKKS_data.EVLA,'ro',markersize = 5,transform = cart.Geodetic(),label='Event Locations (SKS)')
+    # ax.set_xticks([-130,-125,-120,-115,-110,-105,-100], crs=proj)
+    # ax.set_yticks([30,35,40,45,50,55,60], crs=proj)
+    ax3.plot(SKS_data.STLO,SKS_data.STLA,'kv',transform=cart.Geodetic(),markersize=10,label='Station Loction')
     ax3.set_title('{} coverage for Station {}'.format(phase,stat))
     ax3.legend()
     plt.show()
