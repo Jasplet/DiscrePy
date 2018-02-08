1c7085f3ef40e2f5a94116f3c20099b61276e84c
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-08 17:48:22 +0000

    Created shebang and basic headers for program. This is a new program to interact with shebagit add interact_sheba.py

---
+++ Programs/__init__.py
@@ -0,0 +1,10 @@
+#! usr/bin/env/ python
+#import useful modules
+import Split_Measure as sm
+import Split_Read as sr
+import plot
+import pandas as import pd
+import splitwavepy as sw
+import obspy as ob
+import numpy as np
+import matplotlib.pyplot as plt
---
+++ Programs/interact_sheba.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+##############################
+#   Program: interact_sheba.py
+##############################
+#   Author: J. Asplet 
+##############################
+#   A python program to act as a wrapper to sheba.
+#   It read, filters, and trims SAC files from a 
+#   provided list of events and passes each event 
+#   to sheba for shear wave splitting analysis. 
+#   When the analysis is complete the program will
+#   collect the output files for each event and it
+#   will concatenate them together into a summary 
+#   output file! 
+
