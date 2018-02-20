cc2b2b912f0639621e125a3d9670cb791af7d8cb
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-20 11:39:18 +0000

    Removing .pyc files from verions control

--- Programs/__init__.py
+++ Programs/__init__.py
@@ -1,10 +1,6 @@
 #! usr/bin/env/ python
 #import useful modules
-import Split_Measure as sm
-import Split_Read as sr
+import Split_Measure
+import Split_Read
 import plot
-import pandas as import pd
-import splitwavepy as sw
-import obspy as ob
-import numpy as np
-import matplotlib.pyplot as plt
+import interact_sheba
