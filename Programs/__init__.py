62be0fb2c4d43289d003f5de97f5d6b4f48948a9
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-20 18:27:45 +0000

    Commented out imports

--- Programs/__init__.py
+++ Programs/__init__.py
@@ -1,6 +1,6 @@
 #! usr/bin/env/ python
 #import useful modules
-import Split_Measure
-import Split_Read
-import plot
-import interact_sheba
+# import Split_Measure
+# import Split_Read
+# import plot
+# import interact_sheba
