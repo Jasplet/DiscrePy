3b303b8d3d659e0c7427e56a7b273b23138b357a
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-03-15 12:34:07 +0000

    Added multiprocessing tools

--- Programs/sdb_analysis.py
+++ Programs/sdb_analysis.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import obspy
 from obspy import taup
+import os
+from subprocess import call
+from multiprocessing import Pool
 
 
 
@@ -18,7 +21,7 @@ class SDB:
     """
     def __init__(self,sdb,Q_threshold=None,gcarc_threshold=None):
 
-        self._raw = pd.read_csv('{}.sdb'.format(sdb),delim_whitespace=True)
+        self._raw = pd.read_csv('{}.sdb'.format(sdb),delim_whitespace=True,converters={'TIME': lambda x: str(x)})
         #Load raw data from provided sdb file. This is going to be a hidden file as I will parse out useful columns to new attributes depending of provided kwargs
 
          #if kwargs are none:
@@ -31,7 +34,7 @@ class SDB:
         Main Function of the Class
         """
 
-    def match(self,sigma=2):
+    def match(self,file,sigma=2):
         """
         Funntion to see if the SKS and SKKS splititng measurements for a pair of measurements match within error
 
@@ -54,15 +57,15 @@ class SDB:
         ubf_SKKS = SKKS_fast + sigma*SKKS_dfast
 
         lbt_SKS = SKS_tlag - sigma*SKS_dtlag
-        ubt_SKS = SKS_tlag - sigma*SKS_dtlag
+        ubt_SKS = SKS_tlag + sigma*SKS_dtlag
         lbt_SKKS = SKKS_tlag - sigma*SKKS_dtlag
-        ubt_SKKS = SKKS_tlag - sigma*SKKS_dtlag
+        ubt_SKKS = SKKS_tlag + sigma*SKKS_dtlag
 
-        outfile = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/SKS_SKKS_matches.sdb','w+')
-        outfile2 = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/SKS_SKKS_diffs.sdb','w+')
+        outfile = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/{}_matches.sdb'.format(file),'w+')
+        outfile2 = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/{}_diffs.sdb'.format(file),'w+')
         outfile.write('DATE TIME STAT SKS_PP_LAT SKS_PP_LON SKKS_PP_LAT SKKS_PP_LON SKS_FAST SKS_DFAST SKS_TLAG SKS_DTLAG SKKS_FAST SKKS_DFAST SKKS_TLAG SKKS_DTLAG\n')
         outfile2.write('DATE TIME STAT SKS_PP_LAT SKS_PP_LON SKKS_PP_LAT SKKS_PP_LON SKS_FAST SKS_DFAST SKS_TLAG SKS_DTLAG SKKS_FAST SKKS_DFAST SKKS_TLAG SKKS_DTLAG\n')
-        for i in enumerate(SKS_fast):
+        for i,value in enumerate(SKS_fast):
             date = self.sdb.DATE.values[i]
             time = self.sdb.TIME.values[i]
             stat = self.sdb.STAT.values[i]
@@ -70,11 +73,13 @@ class SDB:
             SKS_pp_lon = self.pp.lon_SKS.values[i]
             SKKS_pp_lat = self.pp.lat_SKKS.values[i]
             SKKS_pp_lon = self.pp.lon_SKKS.values[i]
-            if (lbf_SKKS[i] =< SKS_fast[i] =< ubf_SKKS[i] ) and (lbt_SKKS[i] =< SKS_tlag[i] =< ubt_SKKS[i]):
+            #print('{} <= {} <= {} and {} <= {} <= {}'.format(lbf_SKKS[i],SKS_fast[i],ubf_SKKS[i],lbt_SKKS[i],SKS_tlag[i],ubt_SKKS[i]))
+            if (lbf_SKKS[i] <= SKS_fast[i] <= ubf_SKKS[i] ) and (lbt_SKKS[i] <= SKS_tlag[i] <= ubt_SKKS[i]):
                 # Do the Fast and Tlag measured for SKS sit within the error bars for SKKS?
-                if (lbf_SKS[i] <= SKKS_fast[i] =< ubf_SKS[i]) and (lbt_SKS[i] =< SKKS_tlag =< ubt_SKS[i]):
+                #print('{} <= {} <= {} and {} <= {} <= {}'.format(lbf_SKKS[i],SKS_fast[i],ubf_SKKS[i],lbt_SKKS[i],SKS_tlag[i],ubt_SKKS[i]))
+                if (lbf_SKS[i] <= SKKS_fast[i] <= ubf_SKS[i]) and (lbt_SKS[i] <= SKKS_tlag[i] <= ubt_SKS[i]):
                     # Do the Fast and Tlag measured for SKKS sit within the 2-sigma error bars for SKS?
-
+                    print('{} <= {} <= {} and {} <= {} <= {}'.format(lbf_SKS[i],SKKS_fast[i],ubf_SKS[i],lbt_SKS[i],SKKS_tlag[i],ubt_SKS[i]))
                     outfile.write('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {}\n'.format(date,time,stat,SKS_pp_lat,SKS_pp_lon,SKKS_pp_lat,SKKS_pp_lon,SKS_fast[i],SKS_dfast[i],SKS_tlag[i],SKS_dtlag[i],SKKS_fast[i],SKKS_dfast[i],SKKS_tlag[i],SKKS_dtlag[i]))
                 else:
                     outfile2.write('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {}\n'.format(date,time,stat,SKS_pp_lat,SKS_pp_lon,SKKS_pp_lat,SKKS_pp_lon,SKS_fast[i],SKS_dfast[i],SKS_tlag[i],SKS_dtlag[i],SKKS_fast[i],SKKS_dfast[i],SKKS_tlag[i],SKKS_dtlag[i]))
@@ -84,3 +89,33 @@ class SDB:
             #End of the matching If block
 
         outfile.close()
+
+
+
+    def package(self,outdir,mypath='/Users/ja17375/Shear_Wave_Splitting/Sheba/SAC'):
+        '''
+        Function to package the SAC files for the observations made into a new directory (for sharing data and results)
+        '''
+        if os.path.isdir('{}{}'.format(mypath,outdir)) is False:
+            #If outdir doesnt exist then Make it ad underlying structure (an additional SAC directory )
+            os.makedirs('{}{}/SAC'.format(mypath,outdir))
+
+        def mk_sacfiles(path,stat,date,time):
+            '''Funciton to generate sacfile names'''
+            #path='/Users/ja17375/Shear_Wave_Splitting/Sheba/SAC'
+            return '{}/{}/{}_{}_{}*BH?.sac'.format(path,stat,stat,date,time)
+
+        def cp_sacfile(sacfile,path,outdir):
+            '''Function to copy sacfile to the output directory
+            Path - path to the output directorys
+            Outdir - output directory name
+            '''
+            call('cp {} {}/{}/SAC/'.format(sacfile,path,outdir),shell=True)
+            print('cp {} {}/{}/SAC/'.format(sacfile,path,outdir))
+
+        mk_sacfiles_mypath = lambda stat,date,time : mk_sacfiles('/Users/ja17375/Shear_Wave_Splitting/Data/SAC_files',stat,date,time)
+        cp_sacfiles_mypath = lambda sacfile: cp_sacfile(sacfile,mypath,outdir)
+        sacfiles = list(map(mk_sacfiles_mypath,self.sdb.STAT,self.sdb.DATE,self.sdb.TIME))
+
+        for file in sacfiles:
+            cp_sacfile(file,mypath,outdir)
