76a2d069979eb34b9a414bc657b738cc1764ec4e
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-04-26 15:12:21 +0100

    Moved code to make .pairs files here from the SDSanalysis notebook. Added __main__ block

--- Programs/sdb_analysis.py
+++ Programs/sdb_analysis.py
@@ -4,8 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import cartopy.crs as cart
-#import cartopy
+import sys
 import matplotlib.gridspec as gridspec
 import obspy
 from obspy import taup
@@ -13,6 +12,35 @@ import os
 from subprocess import call
 import shlex
 
+def make_pairs(path,sdb_stem):
+    """
+    Function to take .sdb files for SKS and SKKS and generate the set of SKS-SKKS pais that exist in the datasetself.
+    inputs
+    - sdb_stem: [str] sdb file stem for the data that you want to process
+    - path: [str] path to Sheba/Results directory that contains the sdb's that you want
+    -
+
+    """
+    ## Set Path to Sheba Directory
+    SKS_sdb = '{}_SKS_sheba_results.sdb'.format(sdb_stem)
+    SKKS_sdb = '{}_SKKS_sheba_results.sdb'.format(sdb_stem)
+    # First import the SKS and SKKS .sdb files (sdb = splitting database)
+    date_time_convert = {'TIME': lambda x: str(x),'DATE': lambda x : str(x)}
+    SKS = pandas.read_csv('{}/{}'.format(path,SKS_sdb),delim_whitespace=True,converters=date_time_convert)
+    SKKS = pandas.read_csv('{}/{}'.format(path,SKKS_sdb),delim_whitespace=True,converters=date_time_convert)
+    # Now the sdb files have been read as pandas dataframes, we can perform an inner join. This will return a single dataframe containing all rows from SKS and SKKS where
+    # ['DATE','TIME','STAT','STLA','STLO','EVLA','EVLO','EVDP','DIST','AZI','BAZ'] are the same.
+    SKS_SKKS_pair = pandas.merge(SKS,SKKS,on=['DATE','TIME','STAT','STLA','STLO','EVLA','EVLO','EVDP','DIST','AZI','BAZ'],how='inner')
+    relabel = {'FAST_x':'FAST_SKS', 'DFAST_x': 'DFAST_SKS','TLAG_x':'TLAG_SKS','DTLAG_x':'DTLAG_SKS','SPOL_x':'SPOL_SKS','DSPOL_x':'DSPOL_SKS',
+          'WBEG_x':'WBEG_SKS','WEND_x':'WEND_SKS','EIGORIG_x':'EIGORIG_SKS','EIGCORR_x':'EIGCORR_SKS','Q_x':'Q_SKS','SNR_x':'SNR_SKS','NDF_x':'NDF_SKS','FAST_y':'FAST_SKKS', 'DFAST_y': 'DFAST_SKKS',
+          'TLAG_y':'TLAG_SKKS','DTLAG_y':'DTLAG_SKKS','SPOL_y':'SPOL_SKKS','DSPOL_y':'DSPOL_SKKS','WBEG_y':'WBEG_SKKS','WEND_y':'WEND_SKKS','EIGORIG_y':'EIGORIG_SKKS','EIGCORR_y':'EIGCORR_SKKS',
+          'Q_y':'Q_SKKS','SNR_y':'SNR_SKKS','NDF_y':'NDF_SKKS'}
+    # The dictionary relabels the other columns in the join so that we can more easily pick apart the SKS and SKKS results
+    SKS_SKKS_pair.rename(relabel,axis='columns',inplace=True)
+    # Sort the Pairs dataframe so the pairs are in chronological order (by origin time (DATE only))
+    s = SKS_SKKS_pair.sort_values(by=['DATE'],ascending=True)
+    # Save the dataframe to a new pairs file. The stem of the sdb files is used so that is it clear which .pairs file relates to which .sdb files
+    s.to_csv('{}/{}_SKS_SKKS.pairs'.format(path,sdb_stem),sep=' ',index=False)
 
 
 class Pairs:
@@ -22,7 +50,7 @@ class Pairs:
     def __init__(self,pf,Q_threshold=None,gcarc_threshold=None):
 
         self.pairs = pd.read_csv('{}'.format(pf),delim_whitespace=True,converters={'TIME': lambda x: str(x)})
-        #Load raw data from provided sdb file. This is going to be a hidden file as I will parse out useful columns to new attributes depending of provided kwargs
+        #Load raw data from provided .pairs file. This is going to be a hidden file as I will parse out useful columns to new attributes depending of provided kwargs
 
          #if kwargs are none:\
 
@@ -35,12 +63,9 @@ class Pairs:
             self.pp = self.pp = pd.read_csv('{}.pp'.format(pf[:-6]),delim_whitespace=True)
         # Load SDB and PP (pierce points data for a set of SKS-SKKS pairs)
 
-    def main(self):
-        """
-        Main Function of the Class
-        """
 
-    def match(self,file,sigma=2):
+
+    def match(self,path,filestem,sigma=2):
         """
         Funntion to see if the SKS and SKKS splititng measurements for a pair of measurements match within error
 
@@ -67,10 +92,10 @@ class Pairs:
         lbt_SKKS = SKKS_tlag - sigma*SKKS_dtlag
         ubt_SKKS = SKKS_tlag + sigma*SKKS_dtlag
 
-        outfile = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}_matches.pairs'.format(file),'w+')
-        outfile2 = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}_diffs.pairs'.format(file),'w+')
-        mspp1 = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}_matches.mspp'.format(file),'w+')
-        mspp2 = open('/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}_diffs.mspp'.format(file),'w+')
+        outfile = open('{}/{}_matches.pairs'.format(path,filestem),'w+')
+        outfile2 = open('{}/{}_diffs.pairs'.format(path,filestem),'w+')
+        mspp1 = open('{}/{}_matches.mspp'.format(path,filestem),'w+')
+        mspp2 = open('{}/{}_diffs.mspp'.format(path,filestem),'w+')
         outfile.write('DATE TIME STAT STLA STLO EVLA EVLO SKS_PP_LAT SKS_PP_LON SKKS_PP_LAT SKKS_PP_LON SKS_FAST SKS_DFAST SKS_TLAG SKS_DTLAG SKKS_FAST SKKS_DFAST SKKS_TLAG SKKS_DTLAG\n')
         outfile2.write('DATE TIME STAT STLA STLO EVLA EVLO SKS_PP_LAT SKS_PP_LON SKKS_PP_LAT SKKS_PP_LON SKS_FAST SKS_DFAST SKS_TLAG SKS_DTLAG SKKS_FAST SKKS_DFAST SKKS_TLAG SKKS_DTLAG\n')
         for i,value in enumerate(SKS_fast):
@@ -135,3 +160,34 @@ class Pairs:
 
         for file in sacfiles:
             cp_sacfile(file,mypath,outdir)
+
+#####################################################################################################
+# Top level where script is invoked from command line
+if __name__ == '__main__':
+    print('Hello World, I am SDB_analysis.py and now its time to do my thing')
+
+    if len(sys.argv) < 3:
+        print('Excuse me, but you appear to have forgotten to provide enough arguements, 2 arguements (path and sdb_stem) are needed. Lets do this now.')
+        path = input('Input the path to the Sheba/Results directory that contains the sdb files \n > ')
+        sdb_stem = input('Input the stem of the SKS and SKKS sdb files you want to analse \n > ')
+
+
+    elif len(sys.argv == 3):
+        path = sys.argv[1]
+        sdb_stem = sys.argv[2]
+
+    else:
+        Warning('Too many arguements, script will not do anything else')
+
+    if os.path.isfile('{}/{}'.format(path,sdb_stem)) is False:
+        "Lets's find some pairs"
+        make_pairs(path,sdb_stem)
+    else:
+        'Pairs already exist'
+
+    # Now lets read the pairs
+    pair_file = '{}/{}_SKS_SKKS.pairs'.format(path,sdb_stem)
+    p = Pairs(pair_file)
+    p.match(path,pair_file[:-6])
+
+    print('End')
