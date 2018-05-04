00c2e33389ebc767b6ecd23e100c06909e1e6643
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-05-04 15:34:48 +0100

    Added test for if incpetion results file alreayd existd

--- Programs/quality_check.py
+++ Programs/quality_check.py
@@ -13,7 +13,7 @@ import splitwavepy as sw
 import obspy
 import matplotlib.pyplot as plt
 from summary_plot import plotall
-import os
+import os.path
 import SKS_SKKS_qa
 import sys
 
@@ -23,23 +23,51 @@ class Inspecter:
         self.pairs =  pd.read_csv('{}/{}'.format(dir,pairs),delim_whitespace=True,converters={'TIME': lambda x: str(x)})
         self.result_path = dir
         self.qa_dir = '/Users/ja17375/Shear_Wave_Splitting/Sheba/Runs/Jacks_Split/SplitWavePy'
+        self.accepted_i = []
+        self.fs = []
 
-    def write_qa(self,filestem, qual, disc):
+    def write_qa(self,filestem, qual, disc,mode='a+'):
         ''' Writes the event filestem and the quality/discrepancy rating to a textfile'''
 
-        with open('{}/Inspection_Results.txt'.format(self.result_path),'w') as outfile:
-            outfile.write('{} {} {}.format(filestem,qual,disc)' )
+        with open('{}/Inspection_Results.txt'.format(self.result_path),mode) as outfile:
+            outfile.write('{} {} {}\n'.format(filestem,qual,disc) )
 
+
+
+    def run_inspection(self):
+        if os.path.isfile('{}/Inspection_Results.txt'.format(self.result_path)):
+            chk = input('Inspection_Results.txt already exists, [o]verwrite or [c]ontinue? \n >')
+            if chk is 'c':
+                with open('{}/Inspection_Results.txt'.format(self.result_path)) as reader:
+                    head= reader.readline()
+                    for line in reader.readlines():
+                        self.fs = line.split(' ')[0]
+                        self.qual = line.split(' ')[1]
+                        self.disc = ' '.join(line.split(' ')[2:])
+
+                self.loop_thru_pairs()
+        else:
+            print('Initialise Inspection_Results.txt')
+            self.write_qa('Filestem', 'Data Quality', 'Discrepancy',mode='w+')
+            self.loop_thru_pairs()
 
     def loop_thru_pairs(self):
         '''Loops through the pairs and checks if there is a set of QA results'''
         # Write HEaderline to outfile
-        self.write_qa('Filestem', 'Data Quality', 'Discrepancy')
         for i,row in self.pairs.iterrows():
             filestem = '{}_{}_{}'.format(row.STAT,row.DATE,row.TIME)
+            if filestem in self.fs:
+                print("Event already inspected. Qual {} Disc {}".format(self.qual[i],self.disc[i]))
+                qual = self.qual[i]
+                disc = self
             print('Event {} Date: {} Time: {}, Stat: {}'.format(i,row.DATE,row.TIME,row.STAT))
 
-            if row.SNR_SKS < 16 or row.SNR_SKKS < 16:
+            if row.SNR_SKS >= 10 or row.SNR_SKKS >= 10:
+                #Test to see if Signal-to-Noise is too high
+                print('SNR too high, auto-reject')
+                qaul = 'p'
+                disc = 'SNR too high'
+            else:
 
                 sr = (row.FAST_SKS,row.DFAST_SKS,row.TLAG_SKS,row.DTLAG_SKS,row.FAST_SKKS,row.DFAST_SKKS,row.TLAG_SKKS,row.DTLAG_SKKS)
                 print('SHEBA RESULTS: SKS: phi = {} +/- {} dt = {} +/- {}. SKKS: phi = {} +/- {}, dt = {} +/- {}'.format(sr[0],sr[1],sr[2],sr[3],sr[4],sr[5],sr[6],sr[7]))
@@ -52,20 +80,22 @@ class Inspecter:
 
                 qual,disc = plotall('{}/{}'.format(self.qa_dir,filestem))
 
-            else:
-                print('SNR too high, auto-reject')
-                qaul = 'p'
-                disc = 'SNR too high'
-            if qual is not 'p':
-                # I.E. if the data quality is good (or OK)
-                self.accepted_i = i
-                print('Event accepted')
-            self.write_qa(filestem,qual,disc)
-        #Slice the rows which correspond to good data and wirte them to a new pairs file
-        accepted_pairs = self.pairs.iloc[i,:]
-        accepted_pairs.to_csv('{}/Accepted_SKS_SKKS.pairs')
 
-if __name__ == '__main__':
+            self.write_qa(filestem,qual,disc)
+            if 'p' in qual:
+                # I.E. if the data quality is good (or OK)
+                print('Event rejected')
+            else:
+                self.accepted_i.append(i)
+                print('Event accepted')
+
+        #Slice the rows which correspond to good data and wirte them to a new pairs file
+        print('{} accepted'.format(len(self.accepted_i)))
+        accepted_pairs = self.pairs.iloc[self.accepted_i,:]
+        #print(accepted_pairs)
+        accepted_pairs.to_csv('{}/Accepted_SKS_SKKS.pairs'.format(self.result_path),sep=' ',index=False,mode='a+')
+
+if __name__ == '__main__' :
     print('Hello World, this is quality_check.py. You are running me from the command line')
 
     pair_file = sys.argv[1]
@@ -74,5 +104,8 @@ if __name__ == '__main__':
     if len(sys.argv) is 1:
         print('No inputs detected. Please input them now')
         pair_file = input('Input pair filename \n >')
+
     Review = Inspecter(pair_file,results_dir)
-    Review.loop_thru_pairs()
+
+
+    Review.run_inspection()
