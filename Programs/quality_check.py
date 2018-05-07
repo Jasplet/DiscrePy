e4bc5272584a908a1afa9ebe8f4a06e90eeffa04
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-05-07 13:42:48 +0100

    Changed SNR test so that only events with SNR LESS THAN or eq to 10 are rejected

--- Programs/quality_check.py
+++ Programs/quality_check.py
@@ -25,7 +25,8 @@ class Inspecter:
         self.qa_dir = '/Users/ja17375/Shear_Wave_Splitting/Sheba/Runs/Jacks_Split/SplitWavePy'
         self.accepted_i = []
         self.fs = []
-
+        self.qual = []
+        self.disc = []
     def write_qa(self,filestem, qual, disc,mode='a+'):
         ''' Writes the event filestem and the quality/discrepancy rating to a textfile'''
 
@@ -41,11 +42,14 @@ class Inspecter:
                 with open('{}/Inspection_Results.txt'.format(self.result_path)) as reader:
                     head= reader.readline()
                     for line in reader.readlines():
-                        self.fs = line.split(' ')[0]
-                        self.qual = line.split(' ')[1]
-                        self.disc = ' '.join(line.split(' ')[2:])
+                        self.fs.append(line.split(' ')[0])
+                        self.qual.append(line.split(' ')[1])
+                        self.disc.append(' '.join(line.split(' ')[2:]))
+            elif chk is 'o':
+                #Over write
+                self.write_qa('Filestem', 'Data Quality', 'Discrepancy',mode='w+')
 
-                self.loop_thru_pairs()
+            self.loop_thru_pairs()
         else:
             print('Initialise Inspection_Results.txt')
             self.write_qa('Filestem', 'Data Quality', 'Discrepancy',mode='w+')
@@ -56,32 +60,34 @@ class Inspecter:
         # Write HEaderline to outfile
         for i,row in self.pairs.iterrows():
             filestem = '{}_{}_{}'.format(row.STAT,row.DATE,row.TIME)
-            if filestem in self.fs:
-                print("Event already inspected. Qual {} Disc {}".format(self.qual[i],self.disc[i]))
-                qual = self.qual[i]
-                disc = self
-            print('Event {} Date: {} Time: {}, Stat: {}'.format(i,row.DATE,row.TIME,row.STAT))
+            if i < len(self.fs):
+                if filestem == self.fs[i]:
 
-            if row.SNR_SKS >= 10 or row.SNR_SKKS >= 10:
-                #Test to see if Signal-to-Noise is too high
-                print('SNR too high, auto-reject')
-                qaul = 'p'
-                disc = 'SNR too high'
+                    print("Event {} already inspected. Qual {} Disc {}".format(self.fs[i],self.qual[i],self.disc[i]))
+                    qual = self.qual[i]
+                    disc = self
             else:
+                print('Event {} Date: {} Time: {}, Stat: {}'.format(i,row.DATE,row.TIME,row.STAT))
 
-                sr = (row.FAST_SKS,row.DFAST_SKS,row.TLAG_SKS,row.DTLAG_SKS,row.FAST_SKKS,row.DFAST_SKKS,row.TLAG_SKKS,row.DTLAG_SKKS)
-                print('SHEBA RESULTS: SKS: phi = {} +/- {} dt = {} +/- {}. SKKS: phi = {} +/- {}, dt = {} +/- {}'.format(sr[0],sr[1],sr[2],sr[3],sr[4],sr[5],sr[6],sr[7]))
-                #Test if QA results (from SWP) exist
-                if (os.path.isfile('{}/{}_sks.eigm'.format(self.qa_dir,filestem)) and os.path.isfile('{}/{}_skks.xcrm'.format(self.qa_dir,filestem))) is False:
-                    print('QA results do not exist, generating')
-                    SKS_SKKS_qa.measure_sks_skks(filestem,self.qa_dir,[row.WBEG_SKS,row.WEND_SKS,row.WBEG_SKKS,row.WEND_SKKS])
-                    # Now plot the TransM,Xcross and Eigm results
-                    # fig = plt.figure(figsize=(15,10))
+                if row.SNR_SKS <= 10 or row.SNR_SKKS <= 10:
+                    #Test to see if Signal-to-Noise is too high
+                    print('SNR for SKS or SKKS less that 10, auto-reject')
+                    qaul = 'p'
+                    disc = 'SNR <=10'
 
-                qual,disc = plotall('{}/{}'.format(self.qa_dir,filestem))
+                else:
+                    sr = (row.FAST_SKS,row.DFAST_SKS,row.TLAG_SKS,row.DTLAG_SKS,row.FAST_SKKS,row.DFAST_SKKS,row.TLAG_SKKS,row.DTLAG_SKKS)
+                    print('SHEBA RESULTS: SKS: phi = {} +/- {} dt = {} +/- {}. SKKS: phi = {} +/- {}, dt = {} +/- {}'.format(sr[0],sr[1],sr[2],sr[3],sr[4],sr[5],sr[6],sr[7]))
+                    #Test if QA results (from SWP) exist
+                    if (os.path.isfile('{}/{}_sks.eigm'.format(self.qa_dir,filestem)) and os.path.isfile('{}/{}_skks.xcrm'.format(self.qa_dir,filestem))) is False:
+                        print('QA results do not exist, generating')
+                        SKS_SKKS_qa.measure_sks_skks(filestem,self.qa_dir,[row.WBEG_SKS,row.WEND_SKS,row.WBEG_SKKS,row.WEND_SKKS])
+                        # Now plot the TransM,Xcross and Eigm results
+                        # fig = plt.figure(figsize=(15,10))
 
+                    qual,disc = plotall('{}/{}'.format(self.qa_dir,filestem))
 
-            self.write_qa(filestem,qual,disc)
+                self.write_qa(filestem,qual,disc)
             if 'p' in qual:
                 # I.E. if the data quality is good (or OK)
                 print('Event rejected')
