36e9323a2187645cafbb5d173e3a50a945dbd702
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-05-23 11:58:28 +0100

    Created DiscrePy

---
+++ Programs/discre.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# Welcome to discre.py. This script (or maybe module??) is for testing whether
+# SKS SKKS pairs exhibit matching or discrepent splitting. This will be done
+# by using the splitting measured for each phase to correct the splitting on
+# the other.
+### Imports
+import numpy as np
+import pandas as pd
+import obspy as ob
+import splitwavepy as sw
+import matplotlib.pyplot as plt
+# Maybe some others
+
+###############################################################################
+# Intial devel work to be done in devel_disrepy.ipynb
