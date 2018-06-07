77dd31ca2fc990e649de30cb9f20a89fd3c12c7c
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-06-07 16:21:16 +0100

    created pair_stack function

--- Programs/discre.py
+++ Programs/discre.py
@@ -10,8 +10,40 @@ import pandas as pd
 import obspy as ob
 import splitwavepy as sw
 import matplotlib.pyplot as plt
+from stack import Stacker
+from glob import glob
 # Maybe some others
 
 ###############################################################################
-# Intial devel work to be done in devel_disrepy.ipynb
-print('Hello World')
+if __name__ == ' __main__':
+
+    p = pd.read_csv('/Users/ja17375/Shear_Wave_Splitting/Sheba/Resuts/Jacks_Split/Accepted_SKS_SKKS_all.pairs')
+    path = '/Users/ja17375/Shear_Wave_Splitting/Sheba/Runs/Jacks_Split/'
+    pair_stack(p,path)
+    # call function
+
+
+def pair_stack(pairs,path):
+    ''' Runs Stacker for all the desired pairs (a .pairs file)'''
+
+    for i,f in enumerate(pairs.DATE.values):
+        print('It {}, time is {} '.format(i,str(datetime.now())))
+        # First get the right DATE,TIME and STATION
+        date,time,stat = pairs.DATE[i], pairs.TIME[i], pairs.STAT[i]
+        fstem = '{}_{}_{}'.format(stat,date,time)
+
+        sks_lam2 = glob('{}/{}/SKS/{}??_SKS.lam2'.format(path,stat,fstem))
+        if len(sks_lam2) is not 0:
+            # I.e if glob has managed to find the sks lam2 surface file
+            skks_lam2 = glob('{}/{}/SKKS/{}??_SKKS.lam2'.format(path,stat,fstem))
+            Stk = Stacker(sks_lam2,skks_lam2)
+        else:
+            fstem2 = '{}_{}'.format(stat,date)
+            sks_lam2 = glob('{}/{}_*_SKS.lam2'.format(path,fstem2))
+            skks_lam2 = glob('{}/{}_*_SKS.lam2'.format(path,fstem2))
+            # Now for a sanity check
+            if (len(sks_lam2) is not 0) or (len(skks_lam2) is not 0):
+
+            else:
+                print('lam2 surfaces cannot be found, skipping')
+                pass
