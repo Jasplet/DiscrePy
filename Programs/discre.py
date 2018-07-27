0ec12139c57ce0340c7d43637ca5cb97ec6dacbc
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-07-27 10:13:15 +0100

    Tweaked plotting and changed stacking to raw LAM2

--- Programs/discre.py
+++ Programs/discre.py
@@ -22,17 +22,20 @@ from matplotlib import gridspec
 ###############################################################################
 class Tester:
 
-    def __init__(self,pr,path,batch=False):
+    def __init__(self,pr,path,mode='man'):
 
+        self.path =path
 
         date_time_convert = {'TIME': lambda x: str(x),'DATE': lambda x : str(x)}
-        print(pr.split('.')[0])
+        # print(pr.split('.')[0])
         if pr.split('.')[-1] == 'pairs':
-            print('Hello')
+            # print('Hello')
             #File extention is .pairs, lam2 values dont exist so we need to run the stack
             self.pairs = pd.read_csv(pr,delim_whitespace=True,converters=date_time_convert)
             # Assuming that the main pairs filestem was read in, also read the matching and discepent pairs files
             self.lam2 = [ ]
+
+            self.pair_stack(mode)
         elif pr.split('.')[-1] == 'stk':
             # File extention is .stk, lam2 values alreayd exist
             print('.stk files alreayd exist, reading')
@@ -44,13 +47,23 @@ class Tester:
             self.lam2 = self.stk.LAM2.values
         else:
             print('Unrecongised file type')
-        self.path =path
+
         # print(self.path)
 
 
-    def pair_stack(self):
+    def pair_stack(self,mode='man'):
         ''' Runs Stacker for all the desired pairs (a .pairs file)'''
 
+        if mode =='man':
+            ext ='lamR'
+            print('man stacking')
+            rd = input('Enter whick results directory you want the stacks saved under >')
+            out = '/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}/Stacks'.format(rd)
+            if os.path.isdir(out) is False:
+                print('{} does not exist, creating'.format(out))
+                os.mkdir('/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/{}/Stacks'.format(rd))
+        elif mode == 'sheba':
+            ext ='lam2'
         # print('Running')
 
         for i,f in enumerate(self.pairs.DATE.values):
@@ -59,23 +72,31 @@ class Tester:
             date,time,stat = self.pairs.DATE[i], self.pairs.TIME[i], self.pairs.STAT[i]
             fstem = '{}_{}_{}'.format(stat,date,time)
 
-            lam2_stem = glob('{}/{}/SKS/{}??_SKS.lam2'.format(self.path,stat,fstem))
+            lam2_stem = glob('{}/{}/SKS/{}??_SKS.{}'.format(self.path,stat,fstem,ext))
             # print(lam2_stem)
-            # print('{}/{}/SKS/{}??_SKS.lam2'.format(self.path,stat,fstem))
+            print('{}/{}/SKS/{}??_SKS.lam2'.format(self.path,stat,fstem))
             if len(lam2_stem) is not 0:
                 # I.e if glob has managed to find the sks lam2 surface file
-                sks_lam2 = glob('{}/{}/SKS/{}??_SKS.lam2'.format(self.path,stat,fstem))[0]
-                skks_lam2 = glob('{}/{}/SKKS/{}??_SKKS.lam2'.format(self.path,stat,fstem))[0]
-                Stk = Stacker(sks_lam2,skks_lam2,fstem)
-                self.lam2.append(Stk.sol[-1])
+                sks_lam2 = glob('{}/{}/SKS/{}??_SKS.{}'.format(self.path,stat,fstem,ext))[0]
+                skks_lam2 = glob('{}/{}/SKKS/{}??_SKKS.{}'.format(self.path,stat,fstem,ext))[0]
+                Stk = Stacker(sks_lam2,skks_lam2,fstem,out)
+                if mode == 'man':
+                    self.lam2.append(Stk.lam2)
+                elif mode == 'sheba':
+                    self.lam2.append(Stk.sol[-1])
             else:
                 fstem2 = '{}_{}'.format(stat,date)
-                sks_lam2 = glob('{}/{}_*_SKS.lam2'.format(self.path,fstem2))[0]
-                skks_lam2 = glob('{}/{}_*_SKS.lam2'.format(self.path,fstem2))[0]
+                print('fstem2')
+                print('{}/{}/SKS/{}_*_SKS.{}'.format(self.path,stat,fstem2,ext))
+                sks_lam2 = glob('{}/{}/SKS/{}_*_SKS.{}'.format(self.path,stat,fstem2,ext))[0]
+                skks_lam2 = glob('{}/{}/SKKS/{}_*_SKKS.{}'.format(self.path,stat,fstem2,ext))[0]
                 # Now for a sanity check
                 if (len(sks_lam2) is not 0) or (len(skks_lam2) is not 0):
-                    Stk = Stacker(sks_lam2,skks_lam2)
-                    self.lam2.append(Stk.sol[-1])
+                    Stk = Stacker(sks_lam2,skks_lam2,fstem2,out)
+                    if mode == 'man':
+                        self.lam2.append(Stk.lam2)
+                    elif mode == 'sheba':
+                        self.lam2.append(Stk.sol[-1])
                 else:
                     #print('lam2 surfaces cannot be found, skipping')
                     pass
@@ -113,7 +134,7 @@ class Tester:
 
     def hist_lam2(self):
         '''Plot a stacked histogram of lambda 2 values for "discrepent" and "matching" accoridng to classic +/- 2-sigma test'''
-        bins = np.arange(1,2.6,0.2)
+        bins = np.arange(0,1,0.1)
         lambs= [self.stk_matches.LAM2.values,self.stk_diffs.LAM2.values]
 
         fig,ax0 = plt.subplots(1,1,figsize=(8,8))
@@ -164,17 +185,19 @@ class Tester:
             lam2 = self.p_sorted.LAM2.values[s]
             print('Stat {}, Evt Time {}-{} LAM2 = {}'.format(stat,date,time,lam2))
             l_path = '{}/{}/{}_{}_{}'.format(self.path,stat,stat,date,time)
-            print(l_path)
+            # print(l_path)
             self.lam2_surface(l_path)
 
             # fig = plt.figure(figsize=(12,12))
             fig, (ax0,ax1,ax2) = plt.subplots(1,3,figsize=(24,8),sharey=True)
-            plt.suptitle(r'Event {}_{}_{}. Stacked $\lambda _2$ value = {}'.format(stat,date,time,self.p_sorted.LAM2.values[s]),fontsize=16)
+            fig.patch.set_facecolor('None')
+            plt.suptitle(r'Event {}_{}_{}. Stacked $\lambda _2$ value = {:4.3f}'.format(stat,date,time,self.p_sorted.LAM2.values[s]),fontsize=28)
             # gs = gridspec.GridSpec(3,2)
             # ax0 = plt.subplot(gs[0,0])
-            ax0.set_title(r'SKS $\lambda _2$ surfaces')
-            C0 = ax0.contour(self.T,self.F,self.sks_lam2,[0.5,1,2,3,4,5,10,15,20,25,30,40,50],colors='k')
-            ax0.clabel(C0,C0.levels,inline=True,fmt ='%2.0f')
+            ax0.set_title(r'SKS $\lambda _2$ surface',fontsize=24)
+            C0 = ax0.contourf(self.T,self.F,(self.sks_lam2),[0.005,0.01,0.025,0.05,0.075,0.1,0.2,0.3,0.4,0.5,0.75,1.0,1.25,1.5],cmap='inferno_r',extend='both')
+            ax0.contour(C0,colors='k')
+            # ax0.clabel(C0,C0.levels,inline=True,fmt ='%2.3f')
             #Plot SKS Solution
             ax0.plot(lag_sks,fast_sks,'b.',label='SKS Solution')
             ax0.plot([lag_sks-dlag_sks,lag_sks+dlag_sks],[fast_sks,fast_sks],'b-')
@@ -188,10 +211,11 @@ class Tester:
             ax0.set_ylim([-90,90])
             ax0.set_xlim([0,4])
             ax0.set_yticks([-90,-60,-30,0,30,60,90])
-            # ax0.contourf(self.sks_lam2,cmap='magma_r')
+            # ax0.contourf(self.sks_lam2,cmap='inferno_r')
             # ax1 = plt.subplot(gs[0,1])
-            C1 = ax1.contour(self.T,self.F,self.skks_lam2,[0.5,1,2,3,4,5,10,15,20,25,30,40,50],colors='k')
-            ax1.clabel(C1,C1.levels,inline=True,fmt ='%2.0f')
+            C1 = ax1.contourf(self.T,self.F,self.skks_lam2,[0,0.005,0.01,0.025,0.05,0.075,0.1,0.2,0.3,0.4,0.5,0.75,1.0,1.25,1.5],cmap='inferno_r',extend='both')
+            C3 = ax1.contour(C1,colors='k')
+            # ax1.clabel(C1,C1.levels,inline=True,fmt ='%2.3f')
             # ax1.contourf(self.skks_lam2,cmap='magma')
             #Plot SKS Solution
             ax1.plot(lag_sks,fast_sks,'b.',label='SKS Solution')
@@ -205,15 +229,15 @@ class Tester:
             ax1.set_ylim([-90,90])
             ax1.set_xlim([0,4])
             ax1.set_yticks([-90,-60,-30,0,30,60,90])
-            ax1.set_title(r'SKKS $\lambda _2$ surfaces')
+            ax1.set_title(r'SKKS $\lambda _2$ surface',fontsize=24)
             # ax2 = plt.subplot(gs[1:,:])
 
-            self.show_stacks(ax2,'{}/'.format(l_path),l_path.split('/')[-1])
+            self.show_stacks(ax2,l_path.split('/')[-1])
             # print('STK_FAST: {} +/- {}'.format(self.stk_fast,self.stk_dfast))
             # Modify stk_dlag and stk_dfast by sigma
             ##########################
-            self.stk_dlag = self.stk_dlag*sigma
-            self.stk_dfast = self.stk_dfast*sigma
+            # self.stk_dlag = self.stk_dlag*sigma
+            # self.stk_dfast = self.stk_dfast*sigma
             ############################
             ax2.set_ylim([-90,90])
             ax2.set_xlim([0,4])
@@ -228,19 +252,23 @@ class Tester:
             ax2.plot([lag_skks,lag_skks],[fast_skks-dfast_skks,fast_skks+dfast_skks],'r-')
             # Plot Stacked solution on SKS
             ax0.plot(self.stk_lag,self.stk_fast,'g.',label='Stacked Solution')
-            ax0.plot([self.stk_lag- self.stk_dlag, self.stk_lag + self.stk_dlag],[ self.stk_fast, self.stk_fast],'g-')
-            ax0.plot([self.stk_lag, self.stk_lag],[ self.stk_fast - self.stk_dfast ,self.stk_fast +self.stk_dfast],'g-')
+            # ax0.plot([self.stk_lag- self.stk_dlag, self.stk_lag + self.stk_dlag],[ self.stk_fast, self.stk_fast],'g-')
+            # ax0.plot([self.stk_lag, self.stk_lag],[ self.stk_fast - self.stk_dfast ,self.stk_fast +self.stk_dfast],'g-')
             # Plot Stacked Solution on SKKS surface
             ax1.plot(self.stk_lag,self.stk_fast,'g.',label='Stacked Solution')
-            ax1.plot([self.stk_lag-self.stk_dlag,self.stk_lag+self.stk_dlag],[self.stk_fast,self.stk_fast],'g-')
-            ax1.plot([self.stk_lag,self.stk_lag],[self.stk_fast-self.stk_dfast,self.stk_fast+self.stk_dfast],'g-')
+            # ax1.plot([self.stk_lag-self.stk_dlag,self.stk_lag+self.stk_dlag],[self.stk_fast,self.stk_fast],'g-')
+            # ax1.plot([self.stk_lag,self.stk_lag],[self.stk_fast-self.stk_dfast,self.stk_fast+self.stk_dfast],'g-')
             ## Add a legend (on ax0)
             ax0.legend(bbox_to_anchor=(0,1),loc='upper left')
 
-            plt.title('Stacked SKS SKKS surface')
+
+            # cb = fig.colorbar(C1)
+            # cb.add_lines(C3)
+
+            ax2.set_title('Stacked SKS SKKS surface',fontsize=24)
             if save is True:
                 # dir = input('Enter Directory you want to save stacked surfaces to > ')
-                plt.savefig('/Users/ja17375/Shear_Wave_Splitting/Figures/Stacked_Surfaces/{}/LAM2_{:4.4f}_STAT_{}.png'.format(dir,lam2,stat))
+                plt.savefig('/Users/ja17375/Shear_Wave_Splitting/Figures/Stacked_Surfaces/{}/LAM2_{:4.4f}_STAT_{}.eps'.format(dir,lam2,stat),format='eps',dpi=800)
                 plt.close()
 
 
@@ -264,17 +292,26 @@ class Tester:
 
     def lam2_surface(self,fstem):
         ''' Function to read  SKS and SKKS .lam2 surface files from sheba '''
-        sks =glob('{}/{}??_SKS.lam2'.format(fstem,fstem.split('/')[-1]))
-        skks = glob('{}/{}??_SKKS.lam2'.format(fstem,fstem.split('/')[-1]))
-        # print(sks)
-        self.sks_lam2 = np.loadtxt(sks[0],skiprows=4)
-        self.skks_lam2 = np.loadtxt(skks[0],skiprows=4)
+        # print(fstem)
+        t_sks = '{}/SKS/{}??_SKS.lamR'.format('/'.join(fstem.split('/')[0:-1]),fstem.split('/')[-1])
+        t_skks = '{}/SKKS/{}??_SKKS.lamR'.format('/'.join(fstem.split('/')[0:-1]),fstem.split('/')[-1])
+        sks =glob(t_sks)
+        skks = glob(t_skks)
+        # print(t_sks)
+        if len(sks) == 0:
+            stem = '_'.join(fstem.split('/')[-1].split('_')[0:-1]) # aka cut of time par tof file extension
+            # print('{}/SKS/{}*_SKS.lamR'.format('/'.join(fstem.split('/')[0:-1]),fstem.split('/')[-1]))
+            sks = glob('{}/SKS/{}*_SKS.lamR'.format('/'.join(fstem.split('/')[0:-1]),stem))
+            skks = glob('{}/SKKS/{}*_SKKS.lamR'.format('/'.join(fstem.split('/')[0:-1]),stem))
+
+        self.sks_lam2 = np.loadtxt(sks[0])#,skiprows=4) # skip rows not needed for .lamR files
+        self.skks_lam2 = np.loadtxt(skks[0])#,skiprows=4)
 
         nfast,nlag = self.sks_lam2.shape ;
         lag_max = 4.
         [self.T,self.F] = np.meshgrid(np.linspace(0,lag_max,num=nlag),np.arange(-90,91,1)) ;
 
-    def show_stacks(self,ax,path,evt):
+    def show_stacks_sheba(self,ax,path,evt):
         '''Function to find and plot desired surface stacks based on the LAMDA2 value '''
         ### Plot Min Lamnda 2
 
@@ -293,19 +330,55 @@ class Tester:
         lag_max = (nlag) * lag_step ;
         [T,F] = np.meshgrid(np.arange(0,lag_max,lag_step),np.arange(-90,91,1)) ;
 
-        C = ax.contour(T,F,err,[1,2,3,4,5,10,15,20,50,100],colors='k')
+        C = ax.contour(T,F,err,15,colors='k')
         ax.set_ylabel(r'Fast,$\phi$, (deg)')
         ax.set_xlabel(r'Lag ,$\delta$ t, (sec)')
         ax.plot([lag-dlag,lag+dlag],[fast,fast],'g-')
         ax.plot([lag,lag],[fast-dfast,fast+dfast],'g-')
         ax.plot(lag,fast,'g.')
-        ax.clabel(C,C.levels,inline=True,fmt ='%2.0f')
+        ax.clabel(C,C.levels,inline=True,fmt ='%4.3f')
         # ax.set_title(r'Event {}. $\lambda$ 2 value = {}'.format(evt,lam2))
         # Add fast, lag as attributes so we can plot them elsewhere
-        self.stk_fast,self.stk_dfast = fast,dfast
-        self.stk_lag,self.stk_dlag = lag,dlag
+        self.stk_fast,self.stk_dfast
+        self.stk_lag,self.stk_dlag
 
+    def show_stacks(self,ax,evt,path='/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/Jacks_Split_2'):
+        '''Function to find and plot desired surface stacks based on the LAMDA2 value '''
+        ### Plot Min Lamnda 2
+        print('{}/Stacks/{}??.lamSTK'.format(path,evt))
+        file =glob('{}/Stacks/{}??.lamSTK'.format(path,evt))
+        if len(file) == 0:
+            file = glob('{}/Stacks/{}*.lamSTK'.format(path,'_'.join(evt.split('_')[0:-1])))
 
+        stk = np.loadtxt(file[0])
+        nfast,nlag = stk.shape ;
+        lag_step = 0.025
+        lag_max = (nlag) * lag_step;
+        [T,F] = np.meshgrid(np.arange(0,lag_max,lag_step),np.arange(-90,91,1)) ;
+        jf,jt  = np.unravel_index(stk.argmin(),stk.shape)
+        print(stk.argmin())
+        print(jf,jt)
+
+        fast = np.arange(-90,91,1)[jf]
+        lag = np.arange(0,lag_max,lag_step)[jt]
+        C = ax.contourf(T,F,stk,[0.01,0.025,0.05,0.075,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.6,0.7,0.8,0.9,1.0,1.5],cmap='inferno_r',extend='both')
+        C2 = ax.contour(C,colors='k')
+        ax.set_ylabel(r'Fast,$\phi$, (deg)')
+        ax.set_xlabel(r'Lag ,$\delta$ t, (sec)')
+        # ax.plot([lag-dlag,lag+dlag],[fast,fast],'g-')
+        # ax.plot([lag,lag],[fast-dfast,fast+dfast],'g-')
+        ax.plot(lag,fast,'g.')
+        # ax.clabel(C,C.levels,inline=True,fmt ='%4.3f')
+
+        # self.cbar = plt.colorbar(C)
+        # self.cbar.add_lines(C2)
+        # ax.set_title(r'Event {}. $\lambda$ 2 value = {}'.format(evt,lam2))
+        # Add fast, lag as attributes so we can plot them elsewhere
+        sol = stk.min()
+
+        print('Lam2 {}, fast {} lag {}'.format(sol,fast,lag))
+        self.stk_fast = fast
+        self.stk_lag = lag
 
 if __name__ == '__main__':
     print('This is discre.py')
@@ -313,8 +386,8 @@ if __name__ == '__main__':
     # path = '/Users/ja17375/Shear_Wave_Splitting/Sheba/Runs/Jacks_Split'
     p = sys.argv[1]#'/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/Deng/Deng_events_SKS_SKKS.pairs'
     path = sys.argv[2]#'/Users/ja17375/Shear_Wave_Splitting/Sheba/Runs/Deng_events'
-    t = Tester(p,path,batch=True)
-    t.pair_stack()
+    t = Tester(p,path,mode='man')
+
     t.write_lam2(p) # Writes lam2 values to new .stk file
 
     # show_stacks(p2)
