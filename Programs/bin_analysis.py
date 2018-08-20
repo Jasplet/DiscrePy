cb63bdcea14a4f3a1cd93088f8f04c9cf4d9f022
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-08-20 17:16:19 +0100

    Modified plotting to parse out clear split and null events. Also begun to build the if __name__ is __main__ block to allow running from command line

--- Programs/bin_analysis.py
+++ Programs/bin_analysis.py
@@ -9,6 +9,7 @@
 #####################################################################################################
 # Imports
 ##############
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -41,7 +42,7 @@ class Bin:
         # Either save the figure to the output directory or display it now
         if save is True:
             plt.savefig('{}/Analysis_plots_bin_{:04d}_{:03d}_pairs.eps'.format(self.fig_path,self.bn,len(self.bin)),format='eps',transparent=True)
-            plt.close(fig)
+            plt.close('all')
         elif save is False:
             plt.show()
 
@@ -51,17 +52,37 @@ class Bin:
         # fig,(ax1,ax2) = plt.subplots(2,1,sharex=True,figsize = (6,10))
 
         #parse SKS nulls
-        if self.bin.Q_SKS > -0.7:
-            ax1.errorbar(x=self.bin.BAZ,y=self.bin.FAST_SKS,yerr=self.bin.DFAST_SKS,fmt='k.',label='sks')
-        elseif 
-        ax1.errorbar(x=self.bin.BAZ,y=self.bin.FAST_SKKS,yerr=self.bin.DFAST_SKKS,fmt='kx',label='skks')
+        sks_null = self.bin[self.bin.Q_SKS < -0.7]
+        skks_null = self.bin[self.bin.Q_SKKS < -0.7]
+        #parse the clear split results
+        sks_split = self.bin[self.bin.Q_SKS > 0.7]
+        skks_split = self.bin[self.bin.Q_SKKS > 0.7]
+        sks_meh = self.bin.drop(sks_null.index.append(sks_split.index)) # cuts out the clear splits and the clear nulls
+        skks_meh = self.bin.drop(skks_null.index.append(skks_split.index))
+        #plot the OK results
+        # ax1.errorbar(x=sks_meh.BAZ,y=sks_meh.FAST_SKS,yerr=sks_meh.DFAST_SKS,color='blue',fmt='.',label='sks ok')
+        # ax1.errorbar(x=skks_meh.BAZ,y=skks_meh.FAST_SKKS,yerr=skks_meh.DFAST_SKKS,color='blue',fmt='x',label='skks ok')
+        #plot_nulls
+        ax1.errorbar(x=sks_null.BAZ,y=sks_null.FAST_SKS,yerr=sks_null.DFAST_SKS,color='black',fmt='.',label='sks nulls')
+        ax1.errorbar(x=skks_null.BAZ,y=skks_null.FAST_SKKS,yerr=skks_null.DFAST_SKKS,color='black',fmt='x',label='skks nulls')
+        #plot_split
+        ax1.errorbar(x=sks_split.BAZ,y=sks_split.FAST_SKS,yerr=sks_split.DFAST_SKS,color='darkorange',fmt='.',label='sks split')
+        ax1.errorbar(x=skks_split.BAZ,y=skks_split.FAST_SKKS,yerr=skks_split.DFAST_SKKS,color='darkorange',fmt='x',label='skks split')
+
         lim = [np.round(np.min(self.bin.BAZ) - 5),np.round(np.max(self.bin.BAZ) + 5)]
         ax1.set_xlim(lim)
         ax1.set_ylim([-90,90])
 
+        #plot the OK results
+        # ax2.errorbar(x=sks_meh.BAZ,y=sks_meh.FAST_SKKS,yerr=sks_meh.DTLAG_SKS,color='blue',fmt='.',label='sks ok')
+        # ax2.errorbar(x=skks_meh.BAZ,y=skks_meh.FAST_SKKS,yerr=skks_meh.DTLAG_SKKS,color='blue',fmt='x',label='skks ok')
+        #plot_nulls
+        ax2.errorbar(x=sks_null.BAZ,y=sks_null.TLAG_SKS,yerr=sks_null.DTLAG_SKS,color='black',fmt='.',label='sks nulls')
+        ax2.errorbar(x=skks_null.BAZ,y=skks_null.TLAG_SKKS,yerr=skks_null.DTLAG_SKKS,color='black',fmt='x',label='skks nulls')
+        #plot_split
+        ax2.errorbar(x=sks_split.BAZ,y=sks_split.TLAG_SKS,yerr=sks_split.DTLAG_SKS,color='darkorange',fmt='.',label='sks split')
+        ax2.errorbar(x=skks_split.BAZ,y=skks_split.TLAG_SKKS,yerr=skks_split.DTLAG_SKKS,color='darkorange',fmt='x',label='skks split')
 
-        ax2.errorbar(x=self.bin.BAZ,y=self.bin.TLAG_SKS,yerr=self.bin.DTLAG_SKS,fmt='k.')
-        ax2.errorbar(x=self.bin.BAZ,y=self.bin.TLAG_SKKS,yerr=self.bin.DTLAG_SKKS,fmt='kx')
         ax2.set_xlim(lim)
         ax2.set_ylim([0.,4.])
 
@@ -80,9 +101,11 @@ class Bin:
         '''plot a histogram dSI for the bin'''
 
         # fig,ax = plt.subplots(1,1,figsize=(5,5))
-        bins = np.arange(start=0,stop=4.2,step=0.2)
+        bins = np.arange(start=0,stop=3.2,step=0.2)
         h = ax.hist(self.bin.D_SI,bins=bins)
-
+        ax.plot([0.4,0.4],[0,20],color='black',ls='dashed')
+        ylim = [0,max(h[0])]
+        ax.set_ylim(ylim)
         ax.set_ylabel('Frequency')
         ax.set_xlabel(r'$\Delta$SI')
         ax.set_title(r'Histogram of $\Delta$SI ')
@@ -98,11 +121,14 @@ class Bin:
     def plot_lam2(self,ax): #,save=False):
         '''plot a histogram of LAM2 values for the bin'''
         # fig,ax = plt.subplots(1,1,figsize=(6,6))
-        bins = np.arange(start=0,stop=1.1,step=0.1)
+        bins = np.arange(start=0,stop=1.1,step=0.05)
         h = ax.hist(self.bin.LAM2,bins=bins)
+        ylim = [0,max(h[0])]
+        ax.set_ylim(ylim)
         ax.set_ylabel('Frequency')
         ax.set_xlabel(r'$\lambda _2$ value')
         ax.set_title(r'Histogram of $\lambda _2$ values')
+
 
 
         # Either save the figure to the output directory or display it now
@@ -113,16 +139,36 @@ class Bin:
         #     plt.show()
 
 
-    def avg_lam2():
-        '''return average lambda 2 value assuming a guassian distribution'''
+    def avg_lam2(self,lam2):
+        '''return average lambda 2 value assuming a guassian distribution
+            - lam2 : list/np array containing lambda 2 values
 
-    def avg_dSI():
-        '''return average lambda 2 value assuming a gaussian distribution in the bin'''
+        '''
+        # Currently just using numpy.median()
+        avg = np.median(lam2)
+        return avg
 
-    def avg_splitting():
-        '''return average FAST,LAG for the bin. Assuming a gaussian distribution'''
+    def avg_dSI(self,dSI):
+        '''return average lambda 2 value assuming a gaussian distribution in the bin
+            - dSI  : list/np array containing Splitting intensity values
+        '''
+        avg = np.median(dSI)
+        return avg
 
-
+    def avg_splitting(self,fast,lag):
+        '''return average FAST,LAG for the bin. Assuming a gaussian distribution
+            - fast : list/np array of fast directions
+            - lag : list/np array of lag times
+        '''
+        avg_f = np.median(fast)
+        avg_l = np.median(lag)
 
 if __name__ == '__main__':
-  print('Hello I am bin_analysis.py! I am not currently deisgn ed to work from the command line. Please trying importing me into your script or using IPYTHON  :-) ')
+    # print('Hello I am bin_analysis.py! I am not currently deisgn ed to work from the command line. Please trying importing me into your script or using IPYTHON  :-) ')
+    bins_file = sys.argv[1]
+    bf = ps.read_csv(bins_file,delim_whitespace=True,converters={'TIME': lambda x: str(x),'DATE': lambda x : str(x)})
+
+    counts = bins.bin_no.value_counts().copy() # make a dataeframe with the bin number and the count of how many times it occurs
+
+    for i in cts.index:
+        B = Bin(bf,bin_no=i)
